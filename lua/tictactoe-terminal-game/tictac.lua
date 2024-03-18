3222a52828f48e59f1d2feb82de0b853bfe37ec6
unstoppable <internalminds01@gmail.com>
2024-03-18 19:53:38 +0800

    Added Lua Space

---
+++ lua/tictactoe-terminal-game/tictac.lua
@@ -0,0 +1,13 @@
+-- Input for users to select the matrix
+-- Draw the matrix of 3 x 3
+-- User O or X turn to play
+-- Redraw the matrix based on who are the user
+-- check who's the winner on three directions
+-- Draw result if no winner
+
+
+
+
+
+
+
