3dc2a015d0ab717b61fdba406779e9592797cee2
unstoppable <internalminds01@gmail.com>
2024-04-02 08:35:30 +0800

    Crafting player key movement

--- javascript/canvas-fighting-game/index.js
+++ javascript/canvas-fighting-game/index.js
@@ -6,21 +6,30 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
+
 class Sprite {
 	constructor({position, velocity}) {
 		this.position = position
 		this.velocity = velocity
+		this.height = 150
 	}
 
 
 	draw() {
 		c.fillStyle = 'red'
-		c.fillRect(this.position.x, this.position.y, 50, 150)
+		c.fillRect(this.position.x, this.position.y, 50, this.height)
 	}
 
 	update() {
 		this.draw()
+		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
+
+		if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+			this.velocity.y = 0
+		} else this.velocity.y += gravity
+
 	}
 }
 
@@ -45,11 +54,22 @@ const enemy = new Sprite({
 	},
 	velocity: {
 		x: 0,
-		y:0
+		y: 0
 	}
 })
 
 console.log(player)
+
+const keys = {
+	a: {
+		pressed: false
+	},
+	d: {
+		pressed: false
+	}
+}
+
+let lastKey
 
 function animate() {
 	window.requestAnimationFrame(animate)
@@ -57,6 +77,40 @@ function animate() {
 	c.fillRect(0, 0, canvas.width, canvas.height)
 	player.update()
 	enemy.update()
+
+	player.velocity.x = 0
+
+	if (keys.a.pressed && lastKey === 'a') {
+		player.velocity.x = -1
+	} else if (keys.d.pressed && lastKey === 'd') {
+		player.velocity.x = 1
+	}
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+	switch (event.key) {
+		case 'd':
+			keys.d.pressed = true
+			lastKey = 'd'
+			break
+		case 'a':
+			keys.a.pressed = true
+			lastKey = 'a'
+			break
+	}
+	console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+	switch (event.key) {
+		case 'd':
+			keys.d.pressed = false
+			break
+		case 'a':
+			keys.a.pressed = false
+			break
+	}
+	console.log(event.key)
+})
