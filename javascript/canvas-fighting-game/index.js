3d80732787b1563223e33ec3ed27459348a1a5fe
unstoppable <internalminds01@gmail.com>
2024-04-03 10:40:41 +0800

    attack position organized

--- javascript/canvas-fighting-game/index.js
+++ javascript/canvas-fighting-game/index.js
@@ -9,27 +9,59 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-	constructor({position, velocity}) {
+	constructor({position, velocity, color = 'red'}) {
 		this.position = position
 		this.velocity = velocity
+		this.width = 50
 		this.height = 150
 		this.lastKey
+		this.attackBox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y
+			},
+			width: 100,
+			height: 50
+		}
+		this.color = color
+		this.isAttacking
 	}
 
 
 	draw() {
-		c.fillStyle = 'red'
-		c.fillRect(this.position.x, this.position.y, 50, this.height)
+		c.fillStyle = this.color
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+		// Attack box
+		if (this.isAttacking) {
+			c.fillStyle = 'green'
+			c.fillRect(
+				this.attackBox.position.x, 
+				this.attackBox.position.y, 
+				this.attackBox.width, 
+				this.attackBox.height)
+
+		}
 	}
 
 	update() {
 		this.draw()
+		this.attackBox.position.x = this.position.x
+		this.attackBox.position.y = this.position.y
+
 		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
 
 		if (this.position.y + this.height + this.velocity.y >= canvas.height) {
 			this.velocity.y = 0
 		} else this.velocity.y += gravity
+	}
+
+	attack() {
+		this.isAttacking = true
+		setTimeout(() => {
+			this.isAttacking = false
+		}, 100)
 
 	}
 }
@@ -56,7 +88,8 @@ const enemy = new Sprite({
 	velocity: {
 		x: 0,
 		y: 0
-	}
+	},
+	color: 'blue'
 })
 
 console.log(player)
@@ -98,6 +131,17 @@ function animate() {
 	} else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
 		enemy.velocity.x = 1
 	}
+	//detect for collision
+	if (
+		player.attackBox.position.x + player.attackBox.width >= enemy.position.x && 
+		player.attackBox.position.x <= enemy.position.x + enemy.width &&
+		player.attackBox.position.y + player.attackBox.height >= enemy.position.y && 
+		player.attackBox.position.y <= enemy.position.y + enemy.height &&
+		player.isAttacking
+	) {
+		player.isAttacking = false
+		console.log('go')
+	}
 
 }
 
@@ -115,7 +159,10 @@ window.addEventListener('keydown', (event) => {
 			player.lastKey = 'a'
 			break
 		case 'w':
-			player.velocity.y = -10
+			player.velocity.y = -20
+			break
+		case " ":
+			player.attack()
 			break
 
 		case 'ArrowRight':
@@ -127,7 +174,7 @@ window.addEventListener('keydown', (event) => {
 			enemy.lastKey = 'ArrowLeft'
 			break
 		case 'ArrowUp':
-			enemy.velocity.y = -10
+			enemy.velocity.y = -20
 			break
 
 
