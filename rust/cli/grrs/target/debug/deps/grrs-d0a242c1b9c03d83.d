17a1f97ad4f351ccd441d3e3760087782d020b85
unstoppable <internalminds01@gmail.com>
2024-04-14 02:54:33 +0800

    Added Rust projects folder

---
+++ rust/cli/grrs/target/debug/deps/grrs-d0a242c1b9c03d83.d
@@ -0,0 +1,5 @@
+/home/i/xprmnt/rust/cli/grrs/target/debug/deps/grrs-d0a242c1b9c03d83: src/main.rs
+
+/home/i/xprmnt/rust/cli/grrs/target/debug/deps/grrs-d0a242c1b9c03d83.d: src/main.rs
+
+src/main.rs:
---
+++ rust/cli/grrs/target/debug/grrs.d
@@ -0,0 +1,1 @@
+/home/i/xprmnt/rust/cli/grrs/target/debug/grrs: /home/i/xprmnt/rust/cli/grrs/src/main.rs
