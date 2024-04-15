9a62d5bbd6d16be89af339a88dcb4935b59e8084
unstoppable <internalminds01@gmail.com>
2024-04-15 09:45:40 +0800

    code modified

--- rust/cli/grrs/src/main.rs
+++ rust/cli/grrs/src/main.rs
@@ -1,3 +1,8 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
