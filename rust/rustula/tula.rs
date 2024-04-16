42f75d8832d21f572aa1667c3ac2102df491ac8e
unstoppable <internalminds01@gmail.com>
2024-04-17 06:26:24 +0800

    fixing gitignore

--- rust/rustula/tula.rs
+++ rust/rustula/tula.rs
@@ -9,9 +9,23 @@ fn main() -> Result<()> {
         eprintln!("ERROR: could not read file {source_path}: {err}");
     })?;
 
-    let tokens: Vec<_> = source.split(&[' ', '\n']).collect();
+    let tokens: Vec<&str> = source.split(&[' ', '\n']).collect();
+    let mut lexer: &[&str] = &tokens;
 
-    println!("{tokens:?}");
+    println!("{head}", head = lexer[0]);
+
+    println!("{lexer:?}");
+    lexer = &lexer[1..];
+    println!("{lexer:?}");
 
     Ok(())
 }
+
+// define file path
+// read file content and map error
+// define error type
+// token splitter
+// print the result
+// define lexer
+// remove tokens from the left
+// print for debugging
