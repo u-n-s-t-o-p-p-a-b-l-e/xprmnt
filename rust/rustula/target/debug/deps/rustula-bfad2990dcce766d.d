be6d8b42fafce3806823981dd6620ebcf0d53463
unstoppable <internalminds01@gmail.com>
2024-04-17 06:49:13 +0800

    Stop tracking unneccessary files and folders

--- rust/rustula/target/debug/deps/rustula-bfad2990dcce766d.d
+++
@@ -1,5 +0,0 @@
-/home/i/xprmnt/rust/rustula/target/debug/deps/librustula-bfad2990dcce766d.rmeta: src/main.rs
-
-/home/i/xprmnt/rust/rustula/target/debug/deps/rustula-bfad2990dcce766d.d: src/main.rs
-
-src/main.rs:
