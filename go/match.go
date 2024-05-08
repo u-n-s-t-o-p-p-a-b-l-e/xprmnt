8aff1718933ab13bd28738732baec57f9d667134
unstoppable <internalminds01@gmail.com>
2024-05-09 02:30:29 +0800

    Simple switch case in go

---
+++ go/match.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var dayOfWeek int = 3
+
+	switch dayOfWeek {
+		case 1:
+			fmt.Println("Monday")
+		case 2:
+			fmt.Println("Tuesday")
+		case 3:
+			fmt.Println("Wednesday")
+		case 4:
+			fmt.Println("Thursday")
+		case 5:
+			fmt.Println("Friday")
+		case 6:
+			fmt.Println("Saturday")
+		case 7:
+			fmt.Println("Sunday")
+		default:
+			fmt.Println("Invalid day")
+			
+	}
+}
