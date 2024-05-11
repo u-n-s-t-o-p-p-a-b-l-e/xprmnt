c1245138a0236b72a80ad89f923e8d147565b1e9
unstoppable <internalminds01@gmail.com>
2024-05-12 01:19:55 +0800

    Simple CLI greeting in Python

---
+++ python/greet.py
@@ -0,0 +1,7 @@
+def greet_user():
+    print("Welcome to the simple CLI app!")
+    name = input("What's your name? ")
+    age = input("How old are you? ")
+    print(f"Hi, {name}! You are {age} years old.")
+
+greet_user()
