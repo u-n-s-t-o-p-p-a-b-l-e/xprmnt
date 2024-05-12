093cdd4bc8d25742a8f0186d06bfd72e1b5f7913
unstoppable <internalminds01@gmail.com>
2024-05-13 02:20:36 +0800

    All function added

--- typescript/fileman.ts
+++ typescript/fileman.ts
@@ -18,4 +18,38 @@ function deleteFile(filePath: string): void {
 	console.log(`File deleted: ${filePath}`);
 }
 
+function main():  void {
+	const [command, source, destination] = process.argv.slice(2);
+
+	if (!command || !source) {
+		console.log('usage: file-tool <command> <source> [destination]');
+		console.log('Commands: copy, move, delete');
+		return;
+	}
+
+	switch (command.toLowerCase()) {
+		case 'copy':
+			if (!destination) {
+			console.log('Destination path is required for copy command.')
+		}
+		copyFile(source, destination);
+		break;
+		case 'move':
+			if (!destination) {
+			console.log('Destinaton path is required for move command.');
+			return;
+		}
+		moveFile(source, destination);
+		break;
+		case 'delete':
+			deleteFile(source);
+		break;
+		default:
+			console.log('Invalid command. Usage: file-tool <command> <source> [destination]');
+		console.log('Commands: copy, move, delete');
+	}
+}
+
+main();
+
 
