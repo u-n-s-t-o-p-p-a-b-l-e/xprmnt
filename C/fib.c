c4f8017cc9f0d900f78835c6b10665206bad844c
unstoppable <internalminds01@gmail.com>
2024-05-12 19:36:52 +0800

    Simple fibonacci in C

---
+++ C/fib.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void fibonacci(int n) {
+	int first = 0, second = 1, next, i;
+
+	printf("Fibonacci sequence up to %d terms:\n", n);
+	for (i = 0; i < n; i++) {
+		if (i <= 1)
+			next = i;
+		else {
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		printf("%d ", next);
+	}
+	printf("\n");
+}
+
+int main() {
+	int num_terms;
+
+	printf("Enter the number of terms for Fibonacci sequence: ");
+	scanf("%d", &num_terms);
+
+	if (num_terms <= 0) {
+		printf("Error: Plese enter a positive number of terms. \n");
+		return 1;
+	}
+
+	fibonacci(num_terms);
+
+	return 0;
+}
