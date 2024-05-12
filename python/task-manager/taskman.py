6b1826a656964af0d10934be4cf67a9d00bbe3c7
unstoppable <internalminds01@gmail.com>
2024-05-13 01:37:33 +0800

    Add task and view tasks

--- python/task-manager/taskman.py
+++ python/task-manager/taskman.py
@@ -7,4 +7,17 @@ def show_menu():
     print("3. Mark Task as Done")
     print("4. Quit")
 
+def add_task():
+    task = input("Enter the task: ")
+    tasks.append({"task": ttask, "done": False})
+    print("Task added succesfully!")
+
+def view_tasks():
+    if not tasks:
+        print("No tasks added yet.")
+    else:
+        print("Tasks:")
+        for i, task enumerate(tasks, start=1):
+            status = "Done" if task["done"] else "Pending"
+            print(f"{i}. [{status}] {task['task']}")
 
