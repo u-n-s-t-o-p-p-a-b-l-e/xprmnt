ed980517e6ae56d9c0268d1700e9a14d5201c496
unstoppable <internalminds01@gmail.com>
2024-05-12 21:54:45 +0800

    Added Task menu

---
+++ python/task-manager/taskman.py
@@ -0,0 +1,10 @@
+tasks = []
+
+def show_menu():
+    print("\nSimple To-Do List Manager")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Mark Task as Done")
+    print("4. Quit")
+
+
