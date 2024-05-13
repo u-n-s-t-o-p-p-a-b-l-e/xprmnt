62c1c7a17511b46cd17f3cb551ec9f3fae825c5c
unstoppable <internalminds01@gmail.com>
2024-05-14 02:30:51 +0800

    Main function, code completed

--- cpp/inventory.cpp
+++ cpp/inventory.cpp
@@ -22,3 +22,52 @@ void viewInventory(const std::map<std::string, int>& inventory) {
 		std::cout << item.first << ": " << item.second << std::endl;
 	}
 }
+
+int main() {
+	std::map<std::string, int> inventory;
+
+	while (true) {
+		std::cout << "\nInventory Management System\n";
+		std::cout << "1. Add Item\n";
+		std::cout << "2. Remove Item\n";
+		std::cout << "3. View Inventory\n";
+		std::cout << "4. Exit\n";
+		std::cout << "Enter your choice: ";
+
+		int choice;
+		std::cin >> choice;
+
+		switch (choice) {
+			case 1: {
+						std::string item;
+						int quantity;
+						std::cout << "Enter item name: ";
+						std::cin >> item;
+						std::cout << "Enter quantity to add: ";
+						std::cin >> quantity;
+						addItem(inventory, item, quantity);
+						break; 
+					}
+			case 2: {
+						std::string item;
+						int quantity;
+						std::cout << "Enter item name: ";
+						std::cin >> item;
+						std::cout << "Enter quantity to remove: ";
+						std::cin >> quantity;
+						removeItem(inventory, item, quantity);
+						break;
+					}
+			case 3:
+					viewInventory(inventory);
+					break;
+			case 4:
+					std::cout << "Exiting...\n";
+					return 0;
+			default:
+					std::cout << "Invalid choice. Please try again.\n";
+		}
+	}
+
+	return 0;
+}
