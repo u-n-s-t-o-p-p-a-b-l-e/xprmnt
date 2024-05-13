894231247c01873ebca6bef5e0672f6adc5a9f45
unstoppable <internalminds01@gmail.com>
2024-05-14 02:16:43 +0800

    Remove function

--- cpp/inventory.cpp
+++ cpp/inventory.cpp
@@ -6,3 +6,12 @@ void addItem(std::map<std::string, int>& inventory, const std::string& item, int
 	inventory[item] += quantity;
 	std::cout << "Added " << quantity << " " << item << "(s) to inventory." << std::endl;
 }
+
+void removeItem(std::map<std::string, int>& inventory, const std::string& item, int quantity) {
+	if (inventory.find(item) != inventory.end() && inventory[item] >= quantity) {
+		inventory[item] -= quantity;
+		std::cout << "Removed " << quantity << " " << "(s) from inventory." << std::endl;
+	} else {
+		std::cout << "Error: Insufficient quantity of " << item << " in inventory." << std::endl;
+	}
+}
