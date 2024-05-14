7ead6327aafe7a9f58fd8165a4eb8600603c1324
unstoppable <internalminds01@gmail.com>
2024-05-15 02:44:18 +0800

    View Contacts function

--- cpp/contacts.cpp
+++ cpp/contacts.cpp
@@ -7,8 +7,15 @@ struct Contact {
 	std::string phoneNumber;
 };
 
-void addContract(std::map<std::string, Contact>& contacts, const std::string& name, const std::string& phoneNumber) {
+void addContact(std::map<std::string, Contact>& contacts, const std::string& name, const std::string& phoneNumber) {
 	Contact newContact = {name, phoneNumber};
 	contacts[name] = newContact;
 	std::cout << "Contact added: " << name << " (" << phoneNumber << ")" << std::endl;
+}
+
+void viewContacts(const std::map<std::string, Contact>& contacts) {
+	std::cout << "Contacts:\n";
+	for (const auto& pair: contacts) {
+		std::cout << "Name: " << pair.second.name << ", Phone Number: " << pair.second.phoneNumber << std::endl;
+	}
 }
