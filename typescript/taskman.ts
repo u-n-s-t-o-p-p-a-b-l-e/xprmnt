495d1fa0366033d2036b3992ef749c87e9c8657b
unstoppable <internalminds01@gmail.com>
2024-05-14 15:11:05 +0800

    Add Task function

--- typescript/taskman.ts
+++ typescript/taskman.ts
@@ -22,3 +22,12 @@ function loadTasks(): Task[] {
 function saveTasks(tasks: Task[]: void) {
 	fs.writeFileSync(tasksFilePath, JSON.stringify(tasks, null, 2));
 }
+
+function addTasks(description: string): void {
+	const tasks = loadTasks();
+	const id = tasks.length > 0 ? tasks[tasks.length -1].id + 1 : 11;
+	const newTask: Task = { id, description, completed: false };
+	tasks.push(newTask);
+	saveTasks(tasks);
+	console.log('Task added successfully.');
+}
