f4fcb0a344b0a6c19ca0d6db1c307d03903e0d7b
unstoppable <internalminds01@gmail.com>
2024-05-22 00:27:49 +0800

    Convert IPv4 and IPv6 addresses from text to binary form

--- C/client.c
+++ C/client.c
@@ -18,3 +18,11 @@ if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 	printf("\n Socket creation error \n");
 	return -1;
 }
+
+serv_addr.sin_family = AF_INET;
+serv_addr.sin_port = htons(PORT);
+
+if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+	printf("\nInvalid address/ Address not supported\n");
+	return -1;
+}
