1f5b259bf87ce6549f11b408c58f1d376a47a980
unstoppable <internalminds01@gmail.com>
2024-05-22 00:29:42 +0800

    Connect to the server

--- C/client.c
+++ C/client.c
@@ -26,3 +26,8 @@ if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
 	printf("\nInvalid address/ Address not supported\n");
 	return -1;
 }
+
+if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+	printf("\nConnection failed\n");
+	return -1;
+}
