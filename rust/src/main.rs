686115badb19e3cde1ef5fc45767dab9a25682b7
unstoppable <internalminds01@gmail.com>
2024-05-24 00:45:07 +0800

    Add task function

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -16,4 +16,18 @@ struct TaskList {
     tasks: Vec<Task>,
 }
 
+impl Tasklist {
+    fn new() -> Self {
+        TaskList { tasks: Vec::new() }
+    }
 
+    fn add_task(&mut self, title: String) {
+        let id = self.tasks.len() + 1;
+        let task = Task {
+            id,
+            title,
+            completed: false,
+        };
+        self.tasks.push(task);
+    }
+}
