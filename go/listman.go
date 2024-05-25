71b85255b521f6767c03b65bda146ab38e74dd92
unstoppable <internalminds01@gmail.com>
2024-05-25 21:11:12 +0800

    Todo list menu

---
+++ go/listman.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	tasks := make([]string, 0)
+
+	fmt.Println("\nSimple To-Do List Manager")
+	for {
+		fmt.Println("\n1. Add Task")
+		fmt.Println("\n2. View Tasks")
+		fmt.Println("\n3. Exit")
+		fmt.Println("Choose an option: ")
+		
+	}
+	
+}
