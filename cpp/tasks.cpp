1baf2edb65f24be7cd853fa884ad6d480371f6a2
unstoppable <internalminds01@gmail.com>
2024-05-29 04:20:14 +0800

    Completed task function

--- cpp/tasks.cpp
+++ cpp/tasks.cpp
@@ -13,5 +13,12 @@ void addTask(std::vector<Task>& tasks, const std::string& description) {
 	std::cout << "Task added: " << description << std::endl;
 }
 
-
+void completeTask(std::vector<Task>& tasks, size_t index) {
+	if (index >= 0 && index < tasks.size()) {
+		tasks[index].completed = true;
+		std::cout << "Task marked as completed: " << tasks[index].desccription << std::endl;
+	} else {
+		std::cout << "Invalid task index." << std::endl;
+	}
+}
 
