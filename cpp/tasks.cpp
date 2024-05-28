11d2845af4dbf0c2df0772cc6f5a3dc0a3bed8eb
unstoppable <internalminds01@gmail.com>
2024-05-29 04:17:20 +0800

    Add task function

--- cpp/tasks.cpp
+++ cpp/tasks.cpp
@@ -7,5 +7,11 @@ struct Task {
 	bool completed;
 };
 
+void addTask(std::vector<Task>& tasks, const std::string& description) {
+	Task newTask = {description, false};
+	tasks.push_back(newTask);
+	std::cout << "Task added: " << description << std::endl;
+}
+
 
 
