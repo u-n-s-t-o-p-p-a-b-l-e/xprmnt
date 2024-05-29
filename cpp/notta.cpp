dcddd96d716f5e07ff0032fb1a0710cddeffbfe1
unstoppable <internalminds01@gmail.com>
2024-05-30 00:51:45 +0800

    Add note function

--- cpp/notta.cpp
+++ cpp/notta.cpp
@@ -7,3 +7,9 @@ struct Note {
 	std::string title;
 	std::string content;
 };
+
+void addNote(std::vector<Note>& notes, const std::string& title, const std::string& content) {
+	Note newNote = {title, content};
+	notes.push_back(newNote);
+	std::cout << "Note added: " << title << std::endl;
+}
