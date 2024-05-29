1a117648e99aead611f108af2fdb80f0c86cd980
unstoppable <internalminds01@gmail.com>
2024-05-30 00:54:11 +0800

    View note function

--- cpp/notta.cpp
+++ cpp/notta.cpp
@@ -13,3 +13,15 @@ void addNote(std::vector<Note>& notes, const std::string& title, const std::stri
 	notes.push_back(newNote);
 	std::cout << "Note added: " << title << std::endl;
 }
+
+void viewNotes(const std::vector<Note>& notes) {
+	if (notes.empty()) {
+		std::cout << "No notes recorded." << std::endl;
+	} else {
+		std::cout << "Notes:\n";
+		for (const auto& note : notes) {
+			std::cout << "Title: " << note.title << "\n";
+			std::cout << "Content: " << note.content << "\n\n";
+		}
+	}
+}
