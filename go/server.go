cfb9e33692c8c7f2aa55722c6b6aa9cd46551361
unstoppable <internalminds01@gmail.com>
2024-05-31 02:23:24 +0800

    Error handling

--- go/server.go
+++ go/server.go
@@ -13,4 +13,17 @@ func main() {
 		return
 		}
 		defer listener.Close()
+
+		fmt.Println("Server started. Listening on port 8080...")
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Errror accepting connection:", err)
+				continue
+			}
+
+			go handleConnection(conn)
+		}
+		
 }
