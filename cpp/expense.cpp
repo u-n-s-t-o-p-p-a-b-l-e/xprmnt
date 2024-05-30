d98efcb5eebe13de0ace62073e58a49ff60b66a2
unstoppable <internalminds01@gmail.com>
2024-05-31 01:35:25 +0800

    Add expense function

--- cpp/expense.cpp
+++ cpp/expense.cpp
@@ -7,3 +7,9 @@ struct Expense {
 	std::string description;
 	double amount;
 };
+
+void addExpense(std::vector<Expense>& expenses, const std::string& description, double amount) {
+	Expense newExpense = {description, amount};
+	expenses.push_back(newExpense);
+	std::cout << "Expense added: " << description << " ($" << std::fixed << std::setprecision(2) << amount << ")" << std::endl;
+}
