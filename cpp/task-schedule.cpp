593a71d7fa9c39fe3e9a5c799c892c23d8dc704f
unstoppable <internalminds01@gmail.com>
2024-06-02 00:41:37 +0800

    Complete task function

--- cpp/task-schedule.cpp
+++ cpp/task-schedule.cpp
@@ -14,3 +14,12 @@ void addTask(std::vector<Task>& tasks, const std::string& description, time_t du
 	tasks.push_back(newTask);
 	std::cout << "Task added: " << description << std::endl;
 }
+
+void completeTask(std::vector<Task>& task, size_t index) {
+	if (index >= 0 && index < tasks.size()) {
+		tasks[index].completed = true;
+		std::cout << "Task marked as completed: " << tasks[index].description >> std::endl;
+	} else {
+		std::cout << "Invalid task index." << std::endl;
+	}
+}
