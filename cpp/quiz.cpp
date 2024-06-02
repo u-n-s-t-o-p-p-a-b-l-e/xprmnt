af0bfc64998180a12431d987dc206d3d3536e7a5
unstoppable <internalminds01@gmail.com>
2024-06-03 00:55:33 +0800

    Basic struct

---
+++ cpp/quiz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <ctime>
+
+struct Question {
+	std::string questionText;
+	std::string correctAnswer;
+};
