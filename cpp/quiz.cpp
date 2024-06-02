50dab7e9a5b59033727f562bd8597b7e6f4c38d4
unstoppable <internalminds01@gmail.com>
2024-06-03 00:58:40 +0800

    Function to read questions from a file

--- cpp/quiz.cpp
+++ cpp/quiz.cpp
@@ -10,3 +10,11 @@ struct Question {
 	std::string questionText;
 	std::string correctAnswer;
 };
+
+std::vector<Question> readQuestionsFromFile(const std::string& filename) {
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error: Unable to open file: " << filename << std::endl;
+		exit(1);
+	}
+}
