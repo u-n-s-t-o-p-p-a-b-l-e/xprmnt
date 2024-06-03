c03c4ee73b815742bfa8c9c200fda4c21f799ca3
unstoppable <internalminds01@gmail.com>
2024-06-04 01:50:39 +0800

    Exract file name from URL

--- go/filedownloader.go
+++ go/filedownloader.go
@@ -13,4 +13,11 @@ func main() {
 	var url string
 	flag.StringVar(&url, "url", "", "URL of the file to download")
 	flag.Parse()
+
+	if url == "" {
+		fmt.Println("Please provide a URL using the -url flag")
+		return
+	}
+
+	fileName := filepath.Base(url)
 }
