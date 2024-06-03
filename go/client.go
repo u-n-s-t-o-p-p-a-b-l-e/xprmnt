fb4da0ed1e14262873a170078b32a34feb739aa4
unstoppable <internalminds01@gmail.com>
2024-06-04 01:39:44 +0800

    Basic

---
+++ go/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connectiong to server: ", err)
+		return
+	}
+	defer conn.Close()
+}
