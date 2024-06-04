7a9655349f3320983d6df1b374bdf4d7bbd31e7d
unstoppable <internalminds01@gmail.com>
2024-06-05 02:05:13 +0800

    Calculator operations

--- python/cal.py
+++ python/cal.py
@@ -25,3 +25,18 @@ def main():
 
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", substract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    main()
+
