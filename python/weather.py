694393f7a8eae02058ed3f0d63978ce205472e6f
unstoppable <internalminds01@gmail.com>
2024-06-05 02:35:39 +0800

    Weather outputs

--- python/weather.py
+++ python/weather.py
@@ -10,3 +10,19 @@ def get_weather(city):
 def main():
     print("Welcome to the Simple Weather App!")
     city = input("Enter city name: ")
+
+    try:
+        weather_data = get_weather(city)
+        if weather_data["cod"] == 200:
+            print("\nCurrent Weather in", city)
+            print("Description:", weather_data["weather"][0]["desription"])
+            print("Temperature:", weather_data["main"]["temp"], "°C")
+            print("Humidity:", weather_data["main"]["humidity"], "%")
+            print("Wind Speed:", weather_data["wind"]["speed"], "m/s")
+        else:
+            print("City not found. Please enter a valid city name.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+if __name__ == "__main__":
+    main()
