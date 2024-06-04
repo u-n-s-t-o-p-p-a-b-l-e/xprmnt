69514a9a5fdd5697215d1115b89687b87a149063
unstoppable <internalminds01@gmail.com>
2024-06-05 01:59:58 +0800

    Main menu

---
+++ python/cal.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error Division by Zero."
+    else:
+        return x / y
+
+def main():
+    print("Simple Calculator")
+    print("Operations:")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter operation number (1/2/3/4): ")
