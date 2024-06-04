5d0650fe02c5152113a2d61ebf38b1d97bef8dc0
unstoppable <internalminds01@gmail.com>
2024-06-05 02:25:45 +0800

    Setting weather API

---
+++ python/weather.py
@@ -0,0 +1,8 @@
+import requests
+
+def get_weather(city):
+    api_key = "YOUR_API_KEY"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    return data
