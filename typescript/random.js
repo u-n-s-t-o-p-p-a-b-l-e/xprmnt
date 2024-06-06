bc3617a490761947574d4d1906d761b56c116718
unstoppable <internalminds01@gmail.com>
2024-06-07 01:37:16 +0800

    Rendered from ts

---
+++ typescript/random.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var quotes = [
+    { text: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela" },
+    { text: "The way to get started is to quit talking and begin doing.", author: "Walt Disney" },
+    { text: "Your time is limited, don't waste it living someone else's life.", author: "Steve Jobs" },
+    { text: "If life were predictable it would cease to be life, and be without flavor.", author: "Eleanor Roosevelt" },
+    { text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+    { text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.", author: "Mother Teresa" },
+    { text: "In the end, it's not the years in your life that count. It's the life in your years.", author: "Abraham Lincoln" },
+    { text: "Life is either a daring adventure or nothing at all.", author: "Helen Keller" },
+    { text: "The only impossible journey is the one you never begin.", author: "Tony Robbins" },
+    { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" }
+];
+function getRandomQuotes() {
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+function main() {
+    var randomQuote = getRandomQuotes();
+    console.log("\"".concat(randomQuote.text, "\""));
+    console.log("- ".concat(randomQuote.author));
+}
+main();
