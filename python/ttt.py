85c0b7c21dca0c2e09bbafe6ea93fcb10339a77e
unstoppable <internalminds01@gmail.com>
2024-06-08 02:32:53 +0800

    Check diagonals winner

--- python/ttt.py
+++ python/ttt.py
@@ -11,3 +11,10 @@ def check_winner(board):
         for col in range(3):
             if board[0][col] == board[1][col] == board[2][col] != " ":
                 return board[0][col]
+
+        if board[0][0] == board[1][1] == board[2][2] != " ":
+            return board[0][0]
+        if board[0][2] == board[1][1] == board[2][0] != " ":
+            return board[0][2]
+
+        return none
