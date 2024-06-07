f358177e205bf9a58750d39458bc3525912cc7c7
unstoppable <internalminds01@gmail.com>
2024-06-08 02:30:29 +0800

    Check winner rows and columns

--- python/ttt.py
+++ python/ttt.py
@@ -7,3 +7,7 @@ def check_winner(board):
     for row in board:
         if row[0] == row[1] == row[2] != " ":
             return row[0]
+
+        for col in range(3):
+            if board[0][col] == board[1][col] == board[2][col] != " ":
+                return board[0][col]
