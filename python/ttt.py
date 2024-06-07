2be8dc348c205de0edb78e038ad97635b6737227
unstoppable <internalminds01@gmail.com>
2024-06-08 02:28:44 +0800

    Check winner function

--- python/ttt.py
+++ python/ttt.py
@@ -2,3 +2,8 @@ def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 10)
+
+def check_winner(board):
+    for row in board:
+        if row[0] == row[1] == row[2] != " ":
+            return row[0]
