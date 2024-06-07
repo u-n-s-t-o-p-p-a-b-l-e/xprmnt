763319933e4c21cd03ad5d4957c20e637e9bf073
unstoppable <internalminds01@gmail.com>
2024-06-08 02:37:27 +0800

    Main function

--- python/ttt.py
+++ python/ttt.py
@@ -25,3 +25,9 @@ def is_board_full(board):
             if cell == " ":
                 return False
     return True
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    print("Welcome to TicTacToe")
