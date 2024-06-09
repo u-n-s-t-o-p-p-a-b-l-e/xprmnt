eafec3a5ab03bb0d3d4add4f79fc292c9fd5a7d0
unstoppable <internalminds01@gmail.com>
2024-06-10 06:39:09 +0800

    Handling write stream

--- rust/chat/client.rs
+++ rust/chat/client.rs
@@ -7,4 +7,16 @@ fn main() -> io::Result<()> {
     println!("Enter your name: ");
     io::stdin().read_line(&mut name)?;
     name = name.trim().to_string();
+
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
+    stream.write_all(format!("{}\n", name).as_bytes())?;
+
+    let stream_clone = stream.try_clone()?;
+    thread::spawn(move || {
+        handle_read(stream_clone);
+    });
+
+    handle_write(stream);
+
+    Ok(())
 }
