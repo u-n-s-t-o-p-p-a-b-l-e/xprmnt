3decac3ba97abd3467f24d59bf2411a1288171f3
unstoppable <internalminds01@gmail.com>
2024-06-11 00:20:22 +0800

    Add the new client to the clients map

--- go/chat/server.go
+++ go/chat/server.go
@@ -10,5 +10,8 @@ import (
 var clients = make(map[net.Conn]string)
 var messages = make(chan string)
 
-fun handleConnection(conn net.Conn)
-defer conn.Close()
+fun handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	clients[conn] = conn.RemoteAddr().String()
+}
