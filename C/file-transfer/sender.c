5c94415c2913102a52f226625315b75b76a732b2
unstoppable <internalminds01@gmail.com>
2024-06-13 04:10:02 +0800

    Bind socket to port

--- C/file-transfer/sender.c
+++ C/file-transfer/sender.c
@@ -39,4 +39,10 @@ int main() {
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	printf("Server listening on port %d\n", PORT);
 }
