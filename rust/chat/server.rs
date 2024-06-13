4e05dd49af313237117d5bcc88886c9bdb10481c
unstoppable <internalminds01@gmail.com>
2024-06-14 02:08:32 +0800

    Tcp listener

--- rust/chat/server.rs
+++ rust/chat/server.rs
@@ -5,3 +5,26 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 type Clients = Arc<Mutex<HashMap<Strng, TcpStream>>>;
+
+fn main() ->  io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
+
+    println!("Server running on 127.0.0.1:7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let clients = Arc::clone(&clients);
+                thread::Spawn(move || {
+                    handle_client(stream, clients);
+                });
+            }
+            Err(e) => {
+                eprintln!("Eror: {}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
