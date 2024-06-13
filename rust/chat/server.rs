9c7372504272077ed1cb817d50b25a515885f716
unstoppable <internalminds01@gmail.com>
2024-06-14 02:03:30 +0800

    Basic

---
+++ rust/chat/server.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+type Clients = Arc<Mutex<HashMap<Strng, TcpStream>>>;
