e92cb248375ee1c84fba0961418539d1c1aa28a3
unstoppable <internalminds01@gmail.com>
2024-06-16 04:31:58 +0800

    Receive message function

--- C/chat/client.c
+++ C/chat/client.c
@@ -8,4 +8,17 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+void *receive_messages(void *arg) {
+	int sock = *((int *)arg);
+	char buffer[BUFFER_SIZE] = {0};
 
+	while (1) {
+		memset(buffer, 0, BUFFER_SIZE);
+		int valread = read(sock, buffer, BUFFER_SIZE);
+		if (valread > 0) {
+			printf("Server: %s", buffer);
+		}
+	}
+
+	return NULL;
+}
