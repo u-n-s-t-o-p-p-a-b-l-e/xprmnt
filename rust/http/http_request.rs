e22739fba5a0e1bc7e65389970f760a803b7a7fe
unstoppable <internalminds01@gmail.com>
2024-06-18 05:32:10 +0800

    Usage argument

--- rust/http/http_request.rs
+++ rust/http/http_request.rs
@@ -2,3 +2,12 @@ use std::env;
 use std::io::{self, Read, Write};
 use std::net::TcpStream;
 use std::str;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <URL>", args[0]);
+        std::process::exit(1);
+    }
+}
