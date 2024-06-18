193e23cf96d74d7765e6a270c019c634c9cd3bed
unstoppable <internalminds01@gmail.com>
2024-06-19 01:15:05 +0800

    Main argument function

--- rust/file-transfer/client.rs
+++ rust/file-transfer/client.rs
@@ -2,3 +2,11 @@ use std::fs::File;
 use std::io::{self, Read, Write};
 use std::net::TcpStream;
 use std::env;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+}
