1252e939baff8649be06d8751a8ad69372b8837b
unstoppable <internalminds01@gmail.com>
2024-06-19 00:59:48 +0800

    bind listener

--- rust/file-transfer/server.rs
+++ rust/file-transfer/server.rs
@@ -3,4 +3,7 @@ use std::io::{self, Write, Read};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Server listening on port 7878");
+}
