03f15bf9980a136b23ef7f8d849f37f63ac092cf
unstoppable <internalminds01@gmail.com>
2024-06-19 01:02:59 +0800

    Bind listener error handling

--- rust/file-transfer/server.rs
+++ rust/file-transfer/server.rs
@@ -6,4 +6,19 @@ use std::thread;
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
     println!("Server listening on port 7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+            Err(e) => {
+                eprintln!("Failed to accept a client: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
