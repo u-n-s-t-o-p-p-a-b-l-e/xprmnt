3111741e273822dc9d71096995e423ccf971223b
unstoppable <internalminds01@gmail.com>
2024-06-20 16:19:18 +0800

    Main setting listener function

--- rust/http/http_server.rs
+++ rust/http/http_server.rs
@@ -3,3 +3,25 @@ use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStreammm};
 use std::thread;
 use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878");
+    println!("HTTP Server listening on port 7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+            Err(e) => {
+                eprintln!("Failed to accept a client: {}", e);
+            }
+        }
+    }
+
+    Ok(())
+
+
+}
