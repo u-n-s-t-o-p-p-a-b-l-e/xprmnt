b79638a39b04f783e853624c48a35bc92acd8989
unstoppable <internalminds01@gmail.com>
2024-06-21 07:34:58 +0800

    Main listener function

--- rust/chat-multi-threading/chat_server.rs
+++ rust/chat-multi-threading/chat_server.rs
@@ -2,3 +2,10 @@ use std::io::{self, BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
+
+type ClientList = Arc<Mutex<Vec<TcpStream>>>;
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("Chat server listening on port 7878");
+}
