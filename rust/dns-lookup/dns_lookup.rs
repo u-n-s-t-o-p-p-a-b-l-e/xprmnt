bfc9e513a31ae99459e8b870596610c90f7ce186
unstoppable <internalminds01@gmail.com>
2024-06-24 07:58:54 +0800

    Args usage

--- rust/dns-lookup/dns_lookup.rs
+++ rust/dns-lookup/dns_lookup.rs
@@ -2,3 +2,11 @@ use std::env;
 use std::net::ToSocketAddrs;
 use std::net::UdpSocket;
 use std::io::{self, Write};;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() ! = 2 {
+        eprintln!("Usage: {} <domain>", args[0]);
+        std::process::exit(1);
+    }
+}
