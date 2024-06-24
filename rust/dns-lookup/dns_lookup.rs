e2b0d5752aa70b72033d98381d41e3ce3cc04123
unstoppable <internalminds01@gmail.com>
2024-06-24 08:16:32 +0800

    Fixing compiler error and warnings

--- rust/dns-lookup/dns_lookup.rs
+++ rust/dns-lookup/dns_lookup.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::net::ToSocketAddrs;
-use std::net::UdpSocket;
-use std::io::{self, Write};;
+use std::io::{self};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() ! = 2 {
+    if args.len() != 2 {
         eprintln!("Usage: {} <domain>", args[0]);
         std::process::exit(1);
     }
@@ -13,7 +12,7 @@ fn main() -> io::Result<()> {
     let domain = &args[1];
     match dns_lookup(domain) {
         Ok(addresses) => {
-            println!("IP addresses for {}:" domain);
+            println!("IP addresses for {}:", domain);
             for address in addresses {
                 println!("{}", address);
             }
@@ -27,7 +26,7 @@ fn main() -> io::Result<()> {
 fn dns_lookup(domain: &str) ->  io::Result<Vec<String>> {
     let mut addresses = Vec::new();
     let query = format!("{}:53", domain);
-    match query.to_socket_address() {
+    match query.to_socket_addrs() {
         Ok(addrs) => {
             for addr in addrs {
                 addresses.push(addr.to_string());
