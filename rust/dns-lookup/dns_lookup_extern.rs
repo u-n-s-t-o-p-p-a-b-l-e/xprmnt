15b57fda2ead0750bd03d3ac2d77e5046ba75bce
unstoppable <internalminds01@gmail.com>
2024-06-24 08:25:49 +0800

    Arguments usage

--- rust/dns-lookup/dns_lookup_extern.rs
+++ rust/dns-lookup/dns_lookup_extern.rs
@@ -5,4 +5,10 @@ use trust_dns_resolver::config::*;
 use trust_dns_resolver::Resolver;
 use std::io;
 
-
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <domain>", args[0]);
+        std::process::exit(1);
+    }
+}
