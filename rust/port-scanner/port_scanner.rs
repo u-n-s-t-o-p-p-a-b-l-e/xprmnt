b7ae09da61eb76f4a172cef1f1a952d694add689
unstoppable <internalminds01@gmail.com>
2024-06-25 10:51:54 +0800

    Usage

--- rust/port-scanner/port_scanner.rs
+++ rust/port-scanner/port_scanner.rs
@@ -2,3 +2,11 @@ use std::env;
 use std::net::{TcpStream, ToSocketAddrs};
 use std::time::Duration;
 use std::io::{self};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <host> <port-range>", args[0]);
+        std::process::exit(1);
+    }
+}
