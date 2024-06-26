1fdc14cb84d2280c7aa4886e947ef70ba1ff115b
unstoppable <internalminds01@gmail.com>
2024-06-26 11:32:59 +0800

    Square function

--- rust/multi-thread/suares.rs
+++ rust/multi-thread/suares.rs
@@ -3,3 +3,8 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::process;
+
+fn square(n: u32, result: &Arc<Mutex<Vec<u32>>>) {
+   let mut local_results = result.lock().unwrap();
+   local_results.push(n * n);
+}
