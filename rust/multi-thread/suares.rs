958636ba046487cf9255beb5924d0346e6e603ab
unstoppable <internalminds01@gmail.com>
2024-06-26 11:41:21 +0800

    Threads handling

--- rust/multi-thread/suares.rs
+++ rust/multi-thread/suares.rs
@@ -16,4 +16,15 @@ fn main() {
         eprintln!("Usage: {} < numbers separated by spaces>", args[0]);
         process::exit(1);
     }
+
+    let numbers: Vec<u32> = args[1..]
+        .iter()
+        .filter_map(|s| s.parse::<u32>().ok())
+        .collect();
+
+    let result = Arc::new(Mutex::new(vec![]));
+    let thread_count = 4;
+    let chunk_size = (numbers.len() + thread_count - 1) / thread_count;
+
+    let mut threads = vec![];
 }
