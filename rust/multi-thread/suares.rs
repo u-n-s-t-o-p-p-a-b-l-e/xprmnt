2823e500435ad67998ae5b13682a3b2caecc5b8e
unstoppable <internalminds01@gmail.com>
2024-06-26 11:34:44 +0800

    Usage guide

--- rust/multi-thread/suares.rs
+++ rust/multi-thread/suares.rs
@@ -8,3 +8,12 @@ fn square(n: u32, result: &Arc<Mutex<Vec<u32>>>) {
    let mut local_results = result.lock().unwrap();
    local_results.push(n * n);
 }
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} < numbers separated by spaces>", args[0]);
+        process::exit(1);
+    }
+}
