bc33ed7dd9a826299c51fe27df420848627174bb
unstoppable <internalminds01@gmail.com>
2024-06-28 12:50:51 +0800

    Handle client function

--- rust/tcp-echo/tcp_server.rs
+++ rust/tcp-echo/tcp_server.rs
@@ -1,3 +1,19 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                stream.write_all(&buffer[0..n]).unwrap();
+            }
+            Err(e) => {
+                eprintln!("Failed to read from socket: {}", e);
+                break;
+            }
+        }
+    }
+}
