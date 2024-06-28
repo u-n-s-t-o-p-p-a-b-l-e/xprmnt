4075bffb1c919fee9aea35ea63ce3b4b655fbde6
unstoppable <internalminds01@gmail.com>
2024-06-28 13:10:45 +0800

    Input handling

--- rust/tcp-echo/tcp_client.rs
+++ rust/tcp-echo/tcp_client.rs
@@ -4,4 +4,10 @@ use std::io::{self, Write, Read};
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:7878")?;
     println!("Connected to the server");
+
+    let mut input = String::new();
+    println!("Enter a message:");
+    io::stdin().read_line(&mut input)?;
+
+    stream.write_all(input.as_bytes())?;
 }
