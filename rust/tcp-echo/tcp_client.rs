c69db0ebf82b3ddf0f193f46c04c078fee4ee547
unstoppable <internalminds01@gmail.com>
2024-06-28 13:12:34 +0800

    Getting response from server

--- rust/tcp-echo/tcp_client.rs
+++ rust/tcp-echo/tcp_client.rs
@@ -10,4 +10,11 @@ fn main() -> std::io::Result<()> {
     io::stdin().read_line(&mut input)?;
 
     stream.write_all(input.as_bytes())?;
+
+    let mut buffer = [0; 512];
+    let n = stream.read(&mut buffer)?;
+    let response = String::from_utf8_lossy(&buffer[0..n]);
+    println!("Response from server: {}", response);
+
+    Ok(())
 }
