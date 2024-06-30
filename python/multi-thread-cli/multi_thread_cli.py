8029587b56d1b196a6330f0ac77a21e54b26a587
unstoppable <internalminds01@gmail.com>
2024-06-30 14:43:37 +0800

    Argument parser

--- python/multi-thread-cli/multi_thread_cli.py
+++ python/multi-thread-cli/multi_thread_cli.py
@@ -6,3 +6,9 @@ def print_numbers(name, count):
     for i in range(count):
         print(f"Thread {name}: {i}")
         time.sleep(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='A multi-threaded Python CLI app.')
+    parser.add_argument('--threads', type=int, default=2, help='Number of threads to spawn')
+    parser.add_argument('--count', type=int, default=5, help='Count to print up to in each thread')
+    args = parser.parse_args()
