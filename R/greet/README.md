adae314acaec066073836e8051acf18761b38138
unstoppable <internalminds01@gmail.com>
2024-07-02 00:55:07 +0800

    Added readme

---
+++ R/greet/README.md
@@ -0,0 +1,32 @@
+Run R command to start the virtual environment:
+```
+R
+```
+
+
+First, ensure you have the optparse package installed. If not, you can install it using:
+
+```
+install.packages("optparse")
+```
+Exit R by:
+```
+quit()
+```
+
+
+
+Make the file executable by running:
+
+```
+chmod +x greet.R
+``` 
+Run the script from the command line with an argument:
+```
+./greet.R --name="YourName"
+```
+
+You should see the output:
+```
+Hi, YourName!
+```
--- R/greet/greet.R
+++ R/greet/greet.R
@@ -3,8 +3,8 @@
 suppressPackageStartupMessages(library(optparse))
 
 option_list <- list(
-					make_option(c("-n", "--name"), type="character", default=NULL,
-					help="Your name", metavar="character")
+  make_option(c("-n", "--name"), type="character", default=NULL,
+			  help="Your name", metavar="character")
 )
 
 opt_parser <- OptionParser(option_list=option_list)
@@ -12,7 +12,7 @@ opt <- parse_args(opt_parser)
 
 greet_user <- function(name) {
 	if (is.null(name)) {
-		cat("Hello! Please provide your name using -n or --name option.\n")
+		cat("Hi! Please provide your name using -n or --name option.\n")
 	} else {
 		cat("Hi, ", name, "!\n", sep = "")
 	}
