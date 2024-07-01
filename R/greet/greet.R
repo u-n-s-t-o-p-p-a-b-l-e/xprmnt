4c5cfd980d12a95fe1e887835460a156a3ce483d
unstoppable <internalminds01@gmail.com>
2024-07-02 00:43:47 +0800

    Define command line options

--- R/greet/greet.R
+++ R/greet/greet.R
@@ -1,3 +1,8 @@
 #!/usr/bin/env Rscript
 
 suppressPackageStartupMessages(library(optparse))
+
+option_list <- list(
+					make_option(c("-n", "--name"), type="character", default=NULL,
+					help="Your name", metavar="character")
+)
