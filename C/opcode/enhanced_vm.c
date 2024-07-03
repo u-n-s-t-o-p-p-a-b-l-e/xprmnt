7af21b95f03e9d0567ca6968530708c28a96c1c2
unstoppable <internalminds01@gmail.com>
2024-07-04 05:14:57 +0800

    Define opcodes

---
+++ C/opcode/enhanced_vm.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define OP_HALT 0x00
+#define OP_ADD 0x02
+#define OP_SUB 0x03
+#define OP_MUL 0x04
+#define OP_DIV 0x05
+#define OP_LOAD 0x06
+#define OP_PUSH 0x07
+#define OP_POP 0x08
+#define OP_JMP 0x09
+#define OP_JZ 0x0A
+#define OP_JNZ 0x0B
+#define OP_INPUT 0x0C
