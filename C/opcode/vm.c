1bb44b659ae215c798de27dd4add21e721ec68e9
unstoppable <internalminds01@gmail.com>
2024-07-03 16:16:16 +0800

    Define Opcodes

---
+++ C/opcode/vm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define OP_HALT 0x00
+#define OP_ADD 0x01
+#define OP_SUB 0x02
+#define OP_MUL 0x03
+#define OP_DIV 0x04
+#define OP_LOAD 0x05
+#define OP_PRINT 0x06
