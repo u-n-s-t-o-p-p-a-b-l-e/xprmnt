9e7fba888dd63caf08e6f4b624af3d2031965a2c
unstoppable <internalminds01@gmail.com>
2024-07-05 17:12:04 +0800

    Store index

--- rust/high-performance/quicksort.rs
+++ rust/high-performance/quicksort.rs
@@ -13,4 +13,16 @@ fn quicksort(arr: &mut [i32]) {
 
 fn partition(arr: &mut [i32]) ->  usize {
     let len = arr.len();
+    let pivot_index = len / 2;
+    arr.swap(pivot_index, len - 1);
+    let mut store_index = 0;
+    for i in 0..len - 1 {
+        if arr[i] < arr[len -1] {
+            arr.swap(i, store_index);
+            store_index += 1;
+        }
+    }
+    arr.swap(store_index, len -1);
+    store_index
 }
+
