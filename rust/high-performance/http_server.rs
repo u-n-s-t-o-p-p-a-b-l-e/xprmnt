04aa828e781c114f7196099edbe381732a892742
unstoppable <internalminds01@gmail.com>
2024-07-06 14:57:15 +0800

    Handle Client function

--- rust/high-performance/http_server.rs
+++ rust/high-performance/http_server.rs
@@ -1,4 +1,13 @@
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpSream};
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
+
+fn handle_client(mut stream: TcpStream) {
+   let mut buffer = [0; 1024] ;
+   stream.read(&mut buffer).unwrap();
+
+   let response = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHi World!";
+   stream.write(response.as_bytes()).unwrap();
+   stream.flush().unwrap();
+}
