9e20ccaedf046420cdbcf75eb2dc77c6a6a56296
unstoppable <internalminds01@gmail.com>
2024-07-06 14:58:35 +0800

    Main listener incoming function

--- rust/high-performance/http_server.rs
+++ rust/high-performance/http_server.rs
@@ -11,3 +11,8 @@ fn handle_client(mut stream: TcpStream) {
    stream.write(response.as_bytes()).unwrap();
    stream.flush().unwrap();
 }
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server listening on port 7878");
+}
