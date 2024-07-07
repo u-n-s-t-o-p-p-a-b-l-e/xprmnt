a7c747c3d1e4d9e69822802559fa1eb68657d023
unstoppable <internalminds01@gmail.com>
2024-07-07 18:49:18 +0800

    Adding client to test the server code

---
+++ go/concurency/client/client.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	go func() {}
+}
