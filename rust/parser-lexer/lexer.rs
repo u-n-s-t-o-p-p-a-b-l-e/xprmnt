d269d08e3dfc9005c7e014fefcd2f09842f33d1c
unstoppable <internalminds01@gmail.com>
2024-07-08 19:19:35 +0800

    Is keyword function

--- rust/parser-lexer/lexer.rs
+++ rust/parser-lexer/lexer.rs
@@ -77,4 +77,8 @@ impl<'a> Lexer<'a> {
             Token::Identifier(identifier.to_string())
         }
     }
+
+    fn is_keyword(&self, identifier: &str) -> bool {
+        matches!(identifier, "if" | "else" | "while" | "fn")
+    }
 }
