241c50358dbe8163ad73b8cf4184f7f8962a0070
unstoppable <internalminds01@gmail.com>
2024-07-08 19:00:33 +0800

    Impl lexer

--- rust/parser-lexer/lexer.rs
+++ rust/parser-lexer/lexer.rs
@@ -14,3 +14,16 @@ struct Lexer<'a> {
     read_position: usize,
     current_char: Option<char>,
 }
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Self {
+        let mut lexer = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            current_char: None,
+        };
+        lexer.read_char();
+        lexer
+    }
+}
