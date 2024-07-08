65985a5d8fea5e1f5008059854bb7528b63a4be7
unstoppable <internalminds01@gmail.com>
2024-07-09 00:55:50 +0800

    Enum token

--- rust/parser-lexer/parser.rs
+++ rust/parser-lexer/parser.rs
@@ -6,3 +6,13 @@ enum ASTNode {
     While,
     Identifier(String),
 }
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    Identifier(String),
+    Keyword(String),
+    LParen,
+    RParen,
+    Comma,
+    Eof,
+}
