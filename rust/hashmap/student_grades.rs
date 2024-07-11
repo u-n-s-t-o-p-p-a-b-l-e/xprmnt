bdcc2278cfcbe149514a594f7b338cd7377f19e2
unstoppable <internalminds01@gmail.com>
2024-07-12 01:47:04 +0800

    Insert some grades into the hashmap

--- rust/hashmap/student_grades.rs
+++ rust/hashmap/student_grades.rs
@@ -2,4 +2,8 @@ use std::collections::HashMap;
 
 fn main() {
     let mut grades = HashMap::new();
+
+    grades.insert("Alice", 85);
+    grades.insert("Bob", 78);
+    grades.insert("Charlie", 90);
 }
