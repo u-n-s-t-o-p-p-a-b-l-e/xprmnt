b8784bef5c7671c12c549336960179e80cbda13e
unstoppable <internalminds01@gmail.com>
2024-07-11 15:16:42 +0800

    Token enum

---
+++ C/lexer/parser.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef enum {
+	TOKEN_IF,
+	TOKEN_ELSE,
+	TOKEN_IDENTIFIER,
+	TOKEN_LBRACE,
+	TOKEN_RBRACE,
+	TOKEN_ASSIGN,
+	TOKEN_SEMICOLON,
+	TOKEN_EOF,
+	TOKEN_INVALID
+} TokenType;
+
+
