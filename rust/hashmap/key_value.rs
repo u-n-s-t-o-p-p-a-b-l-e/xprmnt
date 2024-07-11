dc1f6711dc726fd3fea950f538633633b5b6367d
unstoppable <internalminds01@gmail.com>
2024-07-12 01:25:21 +0800

    Insert some key-value pairs into the hashmap

--- rust/hashmap/key_value.rs
+++ rust/hashmap/key_value.rs
@@ -2,4 +2,9 @@ use std::collections::HashMap;
 
 fn main() {
     let mut scores: HashMap<String, i32> = HashMap::new();
+
+    scores.insert(k: String::from("Blue"), v: 10);
+    scores.insert(k: String::from("Yellow"), v: 50);
+    scores.insert(k: String::from("Green"), v: 30);
+    
 }
