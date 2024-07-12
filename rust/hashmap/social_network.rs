b3558d441daed8aaa5054eb404bebdfbf93a9402
unstoppable <internalminds01@gmail.com>
2024-07-13 04:25:37 +0800

    Display the social network

--- rust/hashmap/social_network.rs
+++ rust/hashmap/social_network.rs
@@ -6,4 +6,6 @@ fn main() {
     add_friend(&mut social_network, "Alice", "Bob");
     add_friend(&mut social_network, "Alice", "Charlie");
     add_friend(&mut social_network, "Bob", "Dave");
+
+    display_network(&social_network);
 }
