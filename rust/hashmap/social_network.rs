27eb9e69490321b5968a5528f159c7541216ed49
unstoppable <internalminds01@gmail.com>
2024-07-13 04:24:53 +0800

    Adding friends

---
+++ rust/hashmap/social_network.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut social_network = HashMap::new();
+
+    add_friend(&mut social_network, "Alice", "Bob");
+    add_friend(&mut social_network, "Alice", "Charlie");
+    add_friend(&mut social_network, "Bob", "Dave");
+}
