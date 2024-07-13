1f78b3dc331e4a82bb2a2fa2d4354dbd686ced82
unstoppable <internalminds01@gmail.com>
2024-07-14 06:09:03 +0800

    Demonstrate a sleep function

--- rust/time-related/stopwatch.rs
+++ rust/time-related/stopwatch.rs
@@ -4,4 +4,11 @@ fn main() {
     let start = Instant::now();
 
     let sum: u64 = (1..=1_000_000).sum();
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    println!("Sum: {}", sum);
+
+    println!("Sleeping for 2 seconds...");
 }
