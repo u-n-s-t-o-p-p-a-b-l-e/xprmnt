d479a73a0faecdc5558d1376eb2f8d377bcdd702
unstoppable <internalminds01@gmail.com>
2024-07-14 06:25:39 +0800

    Code completed

--- rust/time-related/countdown_timer.rs
+++ rust/time-related/countdown_timer.rs
@@ -1,6 +1,13 @@
-use std::time::Duration;
 use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let countdown = 10;
+
+    for i in (1..=countdown).rev() {
+        println!("Time remaining: {} seconds", i);
+        sleep(Duration::from_secs(1));
+    }
+
+    println!("Time's up!");
 }
