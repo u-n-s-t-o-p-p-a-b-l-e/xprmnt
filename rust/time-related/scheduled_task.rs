d3bcf625212e4c5c49d89de3a2e8d7b7b1fa03b3
unstoppable <internalminds01@gmail.com>
2024-07-14 06:32:58 +0800

    Code completed

--- rust/time-related/scheduled_task.rs
+++ rust/time-related/scheduled_task.rs
@@ -1,8 +1,19 @@
-use std::time::Duration;
 use std::thread;
+use std::time::Duration;
 
 fn scheduled_task() {
     println!("Task executed");
 }
 
-fn main() {}
+fn main() {
+    let delay = Duration::from_secs(5);
+
+    println!("Tas will be executed after 5 seconds...");
+
+    let handle = therad::spawn(move || {
+        thread::sleep(delay);
+        scheduled_task();
+    });
+
+    handle.join().unwrap();
+}
