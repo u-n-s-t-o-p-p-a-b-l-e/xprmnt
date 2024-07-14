2aca1c73a77e102a3399c16e0624e945721a8164
unstoppable <internalminds01@gmail.com>
2024-07-15 06:47:43 +0800

    Get title function

---
+++ rust/lifetime/book.rs
@@ -0,0 +1,14 @@
+struct Book<'a> {
+    title: &'a str,
+    author: &'a str,
+}
+
+impl<'a> Book<'a> {
+    fn get_title(&self) ->  &'a str {
+        self.title
+    }
+
+    fn get_author() {
+        
+    }
+}
