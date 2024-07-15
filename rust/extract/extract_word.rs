c677e1627470e7f01f91e46e8775882929468a15
unstoppable <internalminds01@gmail.com>
2024-07-16 07:16:14 +0800

    Two ways to extract a word

---
+++ rust/extract/extract_word.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let text = "Hi.there";
+
+    let mark: Vec<&str> = text.split('.').collect();
+
+    if let Some(before_dot) = text.get(0) {
+        println!("Word before dot: {}", before_dot);
+    } else {
+        println!("Dot not found.");
+    }
+    // Other way to  do it
+    if let Some(mark2) = text.find('.') {
+        let before_dot2 = &text[..mark2];
+
+        println!("Word before dot: {}", before_dot);
+    } else {
+        println!("Dot not found.");
+    }
+}
