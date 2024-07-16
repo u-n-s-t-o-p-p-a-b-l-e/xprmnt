32c0bdc80a9badba684d97541fdf8756328ff80d
unstoppable <internalminds01@gmail.com>
2024-07-17 07:29:27 +0800

    Using next with filter and map fixed

--- rust/next/next_iter.rs
+++ rust/next/next_iter.rs
@@ -1,6 +1,6 @@
 fn main() {
     let vec = vec![1, 2, 3, 4, 5];
-    let mut iter = vec.iter().filter(|&7x| x % 2 == 0).map(|&x| x * 2);
+    let mut iter = vec.iter().filter(|&&x| x % 2 == 0).map(|&x| x * 2);
 
     while let Some(value) = iter.next() {
         println!("Processed value {}", value);
