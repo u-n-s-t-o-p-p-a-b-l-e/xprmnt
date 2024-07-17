34c1789f332c6700ad192419745d3d121b04f246
unstoppable <internalminds01@gmail.com>
2024-07-18 05:43:11 +0800

    Impl Node

--- rust/pointer/tree_structure.rs
+++ rust/pointer/tree_structure.rs
@@ -4,5 +4,14 @@ use std::rc::Rc;
 #[derive(Debug)]
 struct Node {
     value: i32,
-    children: Vec<Rc<RefCell<Node>>>,
+    children: Vec<Rc<RefCell<Node>>>,q
+}
+
+impl Node {
+    fn new(value: i32) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value,
+            children: vec![],
+        }))
+    }
 }
