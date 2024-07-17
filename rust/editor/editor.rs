ad74baa466363e1aa62ecb21bfc07a61ac1e47a3
unstoppable <internalminds01@gmail.com>
2024-07-17 18:31:15 +0800

    Display a welcome message

--- rust/editor/editor.rs
+++ rust/editor/editor.rs
@@ -4,4 +4,7 @@ use std::process::exit;
 fn main() {
     print!("\x1B[2J\x1B[H");
     io::stdout().flush().unwrap();
+
+    println!("Welcome to Rust Editor!");
+    println!("Press 'q' to quit.");
 }
