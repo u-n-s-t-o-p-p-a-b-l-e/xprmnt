03cffbd3a18a0083224ce417549b312d36be2079
unstoppable <internalminds01@gmail.com>
2024-07-18 05:46:02 +0800

    Print Tree function

--- rust/pointer/tree_structure.rs
+++ rust/pointer/tree_structure.rs
@@ -18,4 +18,12 @@ impl Node {
     fn add_child(parent: &Rc<RefCell<Node>>, child: Rc<RefCell<Node>>) {
         parent.borrow_mut().children.push(child);
     }
+
+    fn print_tree(node: &Rc<RefCell<Node>>, depth: usize) {
+        let indent = " ".repeat(depth * 2);
+        println!("{}{}", indent, node.borrow().value);
+        for child in node.borrow().children.iter() {
+            Node::print_tree(child, depth + 1);
+        }
+    }
 }
