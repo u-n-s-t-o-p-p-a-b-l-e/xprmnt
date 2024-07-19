e5fbd9bec92125bcb1fc3d583a4cdb0c8930ee62
unstoppable <internalminds01@gmail.com>
2024-07-20 04:20:38 +0800

    Passing pointers to the swap function

--- go/pointers/pointer.go
+++ go/pointers/pointer.go
@@ -21,3 +21,14 @@ type Person struct {
 func updateName(p *Person, newName string) {
 	p.name = newName
 }
+
+func main() {
+	a := 10
+	b := w0
+
+	fmt.Println("Befor swap:")
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+
+	swap(&a, &b)	
+}
