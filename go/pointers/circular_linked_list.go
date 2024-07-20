be55ac959e9a66dcc00125afc8ddcf9b1db3afe0
unstoppable <internalminds01@gmail.com>
2024-07-21 00:48:33 +0800

    Append adds a new node to the end of the circular linked list

--- go/pointers/circular_linked_list.go
+++ go/pointers/circular_linked_list.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding"
+	"fmt"
+)
 
 type Node struct {
 	value int
@@ -9,4 +12,19 @@ type Node struct {
 
 type CircularLinkedList struct {
 	head *Node
+}
+
+func (list *CircularLinkedList) append(value int) {
+	newNode := &Node{value: value}
+	if list.head == nil {
+		list.head = newNode
+		newNode.next = list.head
+	} else {
+		current := list.head
+		for current.next != list.head {
+			current = current.next
+		}
+		current.next = newNode
+		newNode.next = list.head
+	}
 }
