846fc91a03405f77d50be192ad9a1a18f671e8ef
unstoppable <internalminds01@gmail.com>
2024-07-23 05:39:34 +0800

    Implement the Shape trait for Rectangle

--- rust/traits/shapes.rs
+++ rust/traits/shapes.rs
@@ -21,3 +21,13 @@ struct Rectangle {
     width: f64,
     height: f64,
 }
+
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * (self.width + self.height)
+    }
+}
