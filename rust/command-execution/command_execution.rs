35a2af4604391c3cb39708697fcbe6c62ba3f5d9
unstoppable <internalminds01@gmail.com>
2024-07-22 22:02:41 +0800

    Convert the output to a string and print it

--- rust/command-execution/command_execution.rs
+++ rust/command-execution/command_execution.rs
@@ -13,5 +13,7 @@ fn main() {
     if output.status.success() {
         let stdout = str::from_utf8(&output.stdout).expect("Failed to parse stdout");
         println!("Command output:\n{}", stdout);
+    } else {
+        let stderr = str::from_utf8(&output.stderr).expect("Failed to parse stderr");
     }
 }
