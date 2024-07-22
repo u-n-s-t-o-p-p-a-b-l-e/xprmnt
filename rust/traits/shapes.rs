b1b0374ca21a75fe4de846f9b68e6429101a7cb4
unstoppable <internalminds01@gmail.com>
2024-07-23 05:36:34 +0800

    Implement the Shape trait for Circle

--- rust/traits/shapes.rs
+++ rust/traits/shapes.rs
@@ -6,3 +6,13 @@ trait Shape {
 struct Circle {
     radius: f64,
 }
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * std::f64::consts::PI * self.radius
+    }
+}
