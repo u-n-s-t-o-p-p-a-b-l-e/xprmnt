7dc00825457b634ad9773c4f8da64bd334d33f6a
unstoppable <internalminds01@gmail.com>
2024-07-26 01:14:57 +0800

    Converting bytes to an integer

--- rust/byte-manipulation/byte_manipulation.rs
+++ rust/byte-manipulation/byte_manipulation.rs
@@ -6,6 +6,9 @@ fn main() {
     bytes[0] = 10;
     bytes[7] = 20;
     println!("Modified bytes: {:?}", bytes);
+
+    let number = u64::from_le_bytes(bytes);
+    println!("Converted to u64: {}", number);
 }
 
 
