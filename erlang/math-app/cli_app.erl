75d8c233575ae4ba790ef472090c20662be347a2
unstoppable <internalminds01@gmail.com>
2024-07-26 06:35:50 +0800

    Loop and command input

--- erlang/math-app/cli_app.erl
+++ erlang/math-app/cli_app.erl
@@ -4,3 +4,9 @@
 start() ->
 	io:format("Welcome to the Enhanced Erlang CLI app!~n"),
 	loop().
+
+loop() -> 
+	io:format("Enter a command (type 'exit' to quit): "),
+	Input = io:get_line(""),
+	Command = string:trim(Input),
+	handle_command(Command).
