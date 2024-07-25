8b2634aa24e651765df8a360c76b098c2062b951
unstoppable <internalminds01@gmail.com>
2024-07-25 21:37:52 +0800

    Handle signal function

--- rust/system-interaction/unix_signal_handling.rs
+++ rust/system-interaction/unix_signal_handling.rs
@@ -7,3 +7,10 @@ use std::io::Write;
 extern "C" {
     fn signal(sig: i32, handler: extern fn(i32)) -> extern fn(i32);
 }
+
+const SIGINT: i32 = 2;
+
+extern fn handle_signal(_: i32) {
+    println!("\nReceived Ctrl+C! Exiting...");
+    std::process::exit(0);
+}
