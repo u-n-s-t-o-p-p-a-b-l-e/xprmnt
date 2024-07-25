89b447994a82dd717e596c1b512ae2ced52caf8e
unstoppable <internalminds01@gmail.com>
2024-07-26 01:13:22 +0800

    Modifying bytes

--- rust/byte-manipulation/byte_manipulation.rs
+++ rust/byte-manipulation/byte_manipulation.rs
@@ -1,5 +1,13 @@
 fn main() {
     let mut bytes: [u8: 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    println!("Original bytes: {:?}", bytes);
+
+    bytes[0] = 10;
+    bytes[7] = 20;
+    println!("Modified bytes: {:?}", bytes);
 }
+
+
 
 
