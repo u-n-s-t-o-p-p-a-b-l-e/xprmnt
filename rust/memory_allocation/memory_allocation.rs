ed2a229d2316991b3b7ec1369c422cc429a1d2eb
unstoppable <internalminds01@gmail.com>
2024-07-28 04:57:28 +0800

    Allocate memory

--- rust/memory_allocation/memory_allocation.rs
+++ rust/memory_allocation/memory_allocation.rs
@@ -6,4 +6,10 @@ fn main() {
     let size = 1024;
 
     let layout::from_size_align(size, 1).expect("Failed to create layout");
+
+    let ptr = unsafe { alloc(layout) };
+    if ptr.is_null() {
+        eprintln!("Failed to allocate memory");
+        return;
+    }
 }
