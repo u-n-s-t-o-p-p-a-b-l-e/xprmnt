9a092de64e0759acf4eee69a7fc0fcd1abb1f505
unstoppable <internalminds01@gmail.com>
2024-07-29 07:01:43 +0800

    Main function

--- rust/box/box_traits.rs
+++ rust/box/box_traits.rs
@@ -7,6 +7,20 @@ struct Cat;
 
 impl Animal for Dog {
     fn speak(&self) {
+        println!("Woof!");
+    }
+}
+
+impl Animal for Cat {
+    fn speak(&self) {
         println!("Meow!");
     }
+}
+
+fn main() {
+    let dog: Box<dyn Animal> = Box::new(Dog);
+    let cat: Box<dyn Animal> = Box::new(Cat);
+
+    dog.speak();
+    cat.speak();
 }
