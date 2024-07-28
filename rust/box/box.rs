a8364e7c9480b857966affc717f9ce1a404a70e6
unstoppable <internalminds01@gmail.com>
2024-07-29 06:44:46 +0800

    Fmt function

--- rust/box/box.rs
+++ rust/box/box.rs
@@ -5,3 +5,15 @@ use std::fmt;
 struct MyError {
     details: String,
 }
+
+impl MyError {
+    fn new(msg: &str) -> MyError {
+        MyError{details: msg.to_string()} 
+    }
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
