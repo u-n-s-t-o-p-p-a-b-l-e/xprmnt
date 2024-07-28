5d26bb6c192c3be43e8b06ba142ac5288f3469ec
unstoppable <internalminds01@gmail.com>
2024-07-29 07:29:54 +0800

    Run rustc and fixed some code

--- rust/box/box_recursive.rs
+++ rust/box/box_recursive.rs
@@ -8,23 +8,29 @@ use Tree::{Node, Empty};
 fn main() {
     let left = Node(1, Box::new(Empty), Box::new(Empty));
     let right = Node(2, Box::new(Empty), Box::new(Empty));
-    let tree = Node(0, Box::new(Empty), Box::new(Empty));
+    let tree = Node(0, Box::new(left), Box::new(right));
 
     trait Shape {
         fn area(&self) -> f64;
     }
 
     struct Circle {
-        radius: f64;
+        radius: f64,
     }
 
     struct Square {
-        side: f64;
+        side: f64,
     }
 
     impl Shape for Circle {
         fn area(&self) -> f64 {
             3.14 * self.radius * self.radius
+        }
+    }
+
+    impl Shape for Square {
+        fn area(&self) -> f64 {
+            self.side * self.side
         }
     }
 
