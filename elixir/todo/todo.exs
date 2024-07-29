30eb65669e7f2b3c63acb5ba9a7336fe9fe4f9e3
unstoppable <internalminds01@gmail.com>
2024-07-30 06:59:33 +0800

    User instruction

--- elixir/todo/todo.exs
+++ elixir/todo/todo.exs
@@ -1,4 +1,20 @@
 defmodule Todo do
   @todo_file "todo_list.txt"
 
-  
+  def main(args) do
+    case args do
+      ["add",  task] ->
+        add_task(task)
+      ["list"]  ->
+        list_tasks()
+      ["complete", String.to_integer(index_str)]
+        complete_task(index)
+      _ ->
+        IO.puts("""
+          Usage:
+          elixir todo.exs add <task>          - Add  a new task
+          elixir todo.exs list                - List all tasks
+          elixir todo.exs complete <index>    - Mark a task as complete
+          """)
+    end
+  end
