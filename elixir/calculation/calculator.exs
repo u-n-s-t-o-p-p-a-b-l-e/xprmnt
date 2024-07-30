02e7ea692a5775355fd47cca75515544a4915551
unstoppable <internalminds01@gmail.com>
2024-07-30 19:12:00 +0800

    Usage handling

---
+++ elixir/calculation/calculator.exs
@@ -0,0 +1,20 @@
+defmodule Calculator do
+  def main(args) do
+    case args do
+      [operation, operand1, operand2] ->
+        operand1 = parse_number(operand1)
+        operand2 = parse_number(operand2)
+        perform_operation(operation, operand1, operand2)
+      _ ->
+        IO.puts("""
+          Usage:
+          elixir calculator.exs <operation> <operand1> <operand2>
+
+          Operations:
+          add       - Add two numbers
+          substract - Substract second number from first
+          Multiply  - Multiply two numbers
+          divide    - Divide first number by second
+          """)
+    end
+  end
