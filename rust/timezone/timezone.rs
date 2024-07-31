c0541fec6c75c0ebc4fad0e8a64ad6579478a893
unstoppable <internalminds01@gmail.com>
2024-08-01 04:06:42 +0800

    Test run with rustc

--- rust/timezone/timezone.rs
+++ rust/timezone/timezone.rs
@@ -3,7 +3,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 fn current_timestamp() -> u64 {
     let start = SystemTime::now();
     let since_the_epoch = start
-        .duration_sincee(UNIX_EPOCH)
+        .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
     since_the_epoch.as_secs()
 }
