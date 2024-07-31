9d2a37e71d8cb256e50dbd46832ba376a2739df4
unstoppable <internalminds01@gmail.com>
2024-08-01 04:02:47 +0800

    Format timestamp function

--- rust/timezone/timezone.rs
+++ rust/timezone/timezone.rs
@@ -7,3 +7,13 @@ fn current_timestamp() -> u64 {
         .expect("Time went backwards");
     since_the_epoch.as_secs()
 }
+
+fn format_timestamp(timestamp: u64, offset_hours: i32) -> String {
+    let adjusted_timestamp = timestamp as i64 + (offset_hours as i64 * 3600);
+    let secs = adjusted_timestamp % 60;
+    let mins = (adjusted_timestamp / 60) % 60;
+    let hours = (adjusted_timestamp / 3600) % 24;
+    let days = adjusted_timestamp / 86400;
+
+    format!("Days since epoch: {}, Time: {:02}:{:02}:{:02}", days, hours, mins, secs)
+}
