d026399f2758b63ae329c220c6b6366dea807bda
unstoppable <internalminds01@gmail.com>
2024-08-01 05:29:48 +0800

    Main function

--- rust/type-system/generic_stack.rs
+++ rust/type-system/generic_stack.rs
@@ -19,3 +19,14 @@ impl<T> Stack<T> {
         self.elements.last()
     }
 }
+
+fn main() {
+    let mut stack: Stack<i32> = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    println!("Top of the stack: {:?}", stack.peek());
+    println!("Popped from stack: {:?}", stack.pop());
+    println!("Top of the stack after pop: {:?}", stack.peek());
+}
