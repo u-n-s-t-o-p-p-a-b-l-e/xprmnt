3f6ebdd3efa72851780d1d96602035924e81fab1
unstoppable <internalminds01@gmail.com>
2024-08-02 05:21:11 +0800

    Implement the Display trait for MyError

--- rust/error-handling/error.rs
+++ rust/error-handling/error.rs
@@ -8,3 +8,12 @@ enum MyError {
     Io(io::Error),
     NotFound,
 }
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MyError::Io(ref err) => write!(f, "IO error: {}", err),
+            MyError::NotFound => write!(f, "File not found"),
+        }
+    }
+}
