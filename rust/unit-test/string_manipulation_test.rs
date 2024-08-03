1d1e6fc9bfff564b6c91983cceb7995da7a0f0ec
unstoppable <internalminds01@gmail.com>
2024-08-03 09:14:26 +0800

    Test modules

--- rust/unit-test/string_manipulation_test.rs
+++ rust/unit-test/string_manipulation_test.rs
@@ -6,3 +6,16 @@ pub fn is_palindrome(s: &str) -> bool {
     let reversed = reverse_string(s);
     s == reversed
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reverse_string() {
+        assert_eq!(reverse_string("hello"), "olleh");
+        assert_eq!(reverse_string("rust"), "tsur");
+        assert_eq!(reverse_string(""), "");
+    }
+}
+
