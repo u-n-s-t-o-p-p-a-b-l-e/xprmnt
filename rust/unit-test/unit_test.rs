71c5bc285e67b215d2d41bc00d56604cc157cb25
unstoppable <internalminds01@gmail.com>
2024-08-03 08:44:58 +0800

    A function to compute the factorial of a number

--- rust/unit-test/unit_test.rs
+++ rust/unit-test/unit_test.rs
@@ -5,3 +5,10 @@ pub fn add(a: i32, b: i32) -> i32 {
 pub fn is_even(n: i32) -> bool {
     n % 2 ==  0
 }
+
+pub fn factorial(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        _ => n * factorial(n -1),
+    }
+}
