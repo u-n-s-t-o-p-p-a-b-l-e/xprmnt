18ffbfa8ac4a7f67c18a1b2095e05f0052d5bfd1
unstoppable <internalminds01@gmail.com>
2024-08-05 06:21:21 +0800

    Example data to compress

---
+++ rust/data-compression/deflate.rs
@@ -0,0 +1,9 @@
+use std::io::{self, Read, Write};
+use std::fs::File;
+
+const MAX_MATCH: usize = 258;
+const MIN_MATCH: usize = 3;
+
+fn main() -> io::Result<()> {
+    let data = b"Example data to compress using the Deflate algorithm in Rust.";
+}
