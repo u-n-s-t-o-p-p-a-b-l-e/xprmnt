daca96d496405002530a280aac60c22f26efe39b
unstoppable <internalminds01@gmail.com>
2024-08-06 04:03:03 +0800

    Pattern handlings

--- rust/regex-rustc/simple_regex.rs
+++ rust/regex-rustc/simple_regex.rs
@@ -4,4 +4,12 @@ fn matches(pattern: &str, text: &str) -> bool {
     if pattern.is_empty() {
         return text.is_empty();
     }
+
+    let first_match = !text.is_empty() && (pattern.chars().nth(0) == text.chars().nth(0) || pattern.chars().nth(0) == Some('.'));
+
+    if pattern.len() >= 2 && pattern.chars().nth(1) == Some('*') {
+        matches(&pattern[2..1], text) || (first_match && matches(pattern, &text[1..]))
+    } else {
+        first_match && matches(&pattern[1..], &text[1..])
+    }
 }
