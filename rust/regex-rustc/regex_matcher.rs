4664707ce49fb7a49366d346f5143ab9b552a6ee
unstoppable <internalminds01@gmail.com>
2024-08-06 04:29:29 +0800

    Pattern handlings

--- rust/regex-rustc/regex_matcher.rs
+++ rust/regex-rustc/regex_matcher.rs
@@ -4,4 +4,15 @@ fn matches_here(pattern: &str, text: &str) -> bool {
     if pattern.is_empty() {
         return true;
     }
+
+    if pattern.len() >= 2 && &pattern[1..2] ==  "*" {
+        return matches_star(pattern.chars().nth(0).unwrap(), &pattern[2..], text);
+    }
+    if pattern == "$" {
+        return text.is_empty();
+    }
+    if !text.is_empty() && (pattern.chars().nth(0) == text.chars().nth(0) || pattern.chars().nth(0) == Some('.')) {
+        return matches_here(&pattern[1..], &text[1..]);
+    }
+    false
 }
