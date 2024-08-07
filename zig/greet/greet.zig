790c2411f35c12d9fd8ccbc1d7ce0c3da0bc73e2
unstoppable <internalminds01@gmail.com>
2024-08-08 05:10:56 +0800

    Print the arguments

--- zig/greet/greet.zig
+++ zig/greet/greet.zig
@@ -3,4 +3,9 @@ const std = @import("std");
 pub fn main() !void {
     const args = try std.process.argAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
+
+    std.debug.print("Number of arguments: {d}\n", .{args.len});
+    for (args, 0..) |arg, i| {
+        std.debug.print("Argument {d}: {s}\n", .{ i, arg });
+    }
 }
