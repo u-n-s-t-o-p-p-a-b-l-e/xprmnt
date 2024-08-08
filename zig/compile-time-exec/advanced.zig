656c699cdbd6de1b9f28e2b1fc0161c957f2d912
unstoppable <internalminds01@gmail.com>
2024-08-09 05:02:23 +0800

    Init and display function

--- zig/compile-time-exec/advanced.zig
+++ zig/compile-time-exec/advanced.zig
@@ -3,5 +3,11 @@ const std = @import("std");
 const MyStruct = struct {
     value:  i32,
 
-    
+    pub fn init(value: i32) MyStruct {
+        return MyStruct{ .value = value };
+    }
+
+    pub fn display(self: MyStruct) void {
+        std.debug.print("Value: {}\n", .{self.value});
+    }
 }
