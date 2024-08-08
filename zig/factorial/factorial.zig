9d1e7ce2952f251bdad2dff9b67bfcb64023a647
unstoppable <internalminds01@gmail.com>
2024-08-09 03:58:05 +0800

    Optionals

--- zig/factorial/factorial.zig
+++ zig/factorial/factorial.zig
@@ -8,3 +8,8 @@ fn factorial(n: u32) u32 {
     if (n == 0) return 1;
     return n * factorial(n - 1);
 }
+
+fn divide(a: i32, b: i32) ?i32 {
+    if (b == 0) return null;
+    return @divTrunc(a, b);
+}
