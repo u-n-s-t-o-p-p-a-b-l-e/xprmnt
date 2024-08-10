95edff99e2fff686d678a51e82ea5ecc23684539
unstoppable <internalminds01@gmail.com>
2024-08-10 22:22:29 +0800

    Use iterator methods to transform and filter the data

--- rust/greet/features.rs
+++ rust/greet/features.rs
@@ -1,5 +1,11 @@
 fn main() {
     let numbers = vec![1, 2, 3, 4, 5];
 
-    
+    let sum: i32 = numbers
+        .iter()
+        .filter(|&x| x % 2 == 0)
+        .map(|&x| x * x)
+        .sum();
+
+    println!("Sum of squares of even numbers: {}", sum);
 }
