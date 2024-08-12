ff151106e65287b8eaef78a3845f178d9fc300c4
unstoppable <internalminds01@gmail.com>
2024-08-12 16:23:50 +0800

    Struct block

--- rust/blockchain/blockchain.rs
+++ rust/blockchain/blockchain.rs
@@ -2,4 +2,14 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use std::fmt::{self, Write};
 use std::collections::VecDeque;
 
+#[derive(Debug, Clone)]
+struct Block {
+    index: u64,
+    timestamp: u128,
+    data: String,
+    previous_hash: String,
+    hash: String,
+    nonce: u64,
+}
+
 
