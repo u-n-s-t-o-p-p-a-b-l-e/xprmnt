aeb4487f0012591bfe41347091aa1ba3ac1ab391
unstoppable <internalminds01@gmail.com>
2024-08-13 03:46:17 +0800

    Read file function

--- rust/basic/basic.rs
+++ rust/basic/basic.rs
@@ -7,3 +7,19 @@ enum FileError {
     PermissionDenied(String),
     Unknown(String),
 }
+
+fn read_file(path: &str) -> Result<String, FileError> {
+    let path = Path::new(path);
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(e) => match e.kind() {
+            io::ErrorKind::NotFound => {
+                return Err(FileError::NotFound(path.display().to_string()))
+            }
+            io::ErrorKind::NotFound => {
+                return Err(FileError::PermissionDenied(path.display().to_string()))
+            }
+            _ => return Err(FileError::Unknown(e.to_string())),
+        },
+    };
+}
