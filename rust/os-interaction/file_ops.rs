77373c094d597e2ca50ae653b739d1c8c32556af
unstoppable <internalminds01@gmail.com>
2024-08-13 17:21:14 +0800

    Test run with rustc

--- rust/os-interaction/file_ops.rs
+++ rust/os-interaction/file_ops.rs
@@ -4,7 +4,7 @@
 
 extern "C" {
     fn open(pathname: *const u8, flags: i32, mode: i32) -> i32;
-    fn write(fd: i32, buf: *const 8, count: usize) -> isize;
+    fn write(fd: i32, buf: *const u8, count: usize) -> isize;
     fn close(fd: i32) -> i32;
 }
 
