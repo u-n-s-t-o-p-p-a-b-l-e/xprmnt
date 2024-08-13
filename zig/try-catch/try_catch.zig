372dee805f9869e1cf2666b3c6fe4bba2ea4e18d
unstoppable <internalminds01@gmail.com>
2024-08-14 07:37:41 +0800

    Read file function

--- zig/try-catch/try_catch.zig
+++ zig/try-catch/try_catch.zig
@@ -4,3 +4,9 @@ const FileError = error {
     NotFound,
     PermissionDenied,
 };
+
+fn readFile(path: []const u8) FileError![]const u8 {
+    if (std.mem.eql(u8, path, "secret.txt")) {
+        return FileError.PermissionDenied;
+    }
+}
