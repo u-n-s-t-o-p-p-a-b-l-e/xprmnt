894e71a84bf4d83e7be326e43097410d5ebfa990
unstoppable <internalminds01@gmail.com>
2024-08-14 07:39:22 +0800

    File contents

--- zig/try-catch/try_catch.zig
+++ zig/try-catch/try_catch.zig
@@ -9,4 +9,8 @@ fn readFile(path: []const u8) FileError![]const u8 {
     if (std.mem.eql(u8, path, "secret.txt")) {
         return FileError.PermissionDenied;
     }
+    if (std.mem.eql(u8, path, "missing.txt")) {
+        return FileError.NotFound;
+    }
+    return "File contents";
 }
