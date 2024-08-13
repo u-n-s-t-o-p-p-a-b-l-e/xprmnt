cb8126fc927ee113fc97583dca91c2267e8971b1
unstoppable <internalminds01@gmail.com>
2024-08-13 17:07:32 +0800

    Write only mode, create file, read and write permission

--- rust/os-interaction/file_ops.rs
+++ rust/os-interaction/file_ops.rs
@@ -7,3 +7,8 @@ extern "C" {
     fn write(fd: i32, buf: *const 8, count: usize) -> isize;
     fn close(fd: i32) -> i32;
 }
+
+const O_WRONLY: i32 = 1;
+const O_CREAT: i32 = 64;
+const S_IRUSR: i32 = 0o400;
+const S_IWUSR: i32 = 0o200;
