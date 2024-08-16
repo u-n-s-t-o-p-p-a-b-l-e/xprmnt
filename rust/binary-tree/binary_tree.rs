c5acef303c367958f55b50c739e0a5c9bfd8deb1
unstoppable <internalminds01@gmail.com>
2024-08-16 23:03:25 +0800

    Insert function

--- rust/binary-tree/binary_tree.rs
+++ rust/binary-tree/binary_tree.rs
@@ -6,3 +6,25 @@ struct Node<T> {
     left: Option<Box<Node<T>>>,
     right: Option<Box<Node<T>>>,
 }
+
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Node {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(value);
+                } else {
+                    self.left = Some(Box::new(Node::new(value)));
+                }
+            }
+        }
+    }
+}
