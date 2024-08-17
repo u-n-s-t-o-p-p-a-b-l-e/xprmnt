78bcc28ac64cf52b7fa6453470cb0202aeecd33a
unstoppable <internalminds01@gmail.com>
2024-08-17 12:24:53 +0800

    Enum expr

---
+++ rust/interpreter/interpreter.rs
@@ -0,0 +1,8 @@
+#[allow(dead_code)]
+enum Expr {
+    Number(i32),
+    Add(Box<Expr>, Box<Expr>),
+    Subtract(Box<Expr>, Box<Expr>),
+    Multiply(Box<Expr>, Box<Expr>),
+    Divide(Box<Expr>, Box<Expr>),
+}
