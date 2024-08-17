36f0c1135f7f6b3b3770f9a0c380fd328dc8d9d5
unstoppable <internalminds01@gmail.com>
2024-08-17 12:16:27 +0800

    Impl shape for circle and rectangle

--- rust/polymorphism/polymorphism.rs
+++ rust/polymorphism/polymorphism.rs
@@ -10,3 +10,15 @@ struct Rectangle {
     width: f64,
     height: f64,
 }
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        3.14159 * self.radius * self.radius
+    }
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+}
