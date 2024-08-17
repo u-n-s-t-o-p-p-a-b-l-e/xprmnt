1bb45fed5e0c2fe052b578669a6269651bd9d603
unstoppable <internalminds01@gmail.com>
2024-08-17 12:01:21 +0800

    Lexer function

--- rust/lexer/simple_lexer.rs
+++ rust/lexer/simple_lexer.rs
@@ -7,3 +7,16 @@ enum Token {
     Divide,
     EOF,
 }
+
+fn lexer(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut chars = input.chars().peekable();
+
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '0'..='9' => {
+                let mut num = 0;
+            }
+        }
+    }
+}
