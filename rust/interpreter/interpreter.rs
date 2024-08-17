d509a2ac19cf4a027959527bd8bcc6793a8a9e87
unstoppable <internalminds01@gmail.com>
2024-08-17 12:27:29 +0800

    Eval function

--- rust/interpreter/interpreter.rs
+++ rust/interpreter/interpreter.rs
@@ -6,3 +6,13 @@ enum Expr {
     Multiply(Box<Expr>, Box<Expr>),
     Divide(Box<Expr>, Box<Expr>),
 }
+
+fn eval(expr: &Expr) -> i32 {
+    match expr {
+        Expr::Number(n) => *n,
+        Expr::Add(lhs, rhs) => eval(lhs) + eval(rhs),
+        Expr::Subtract(lhs, rhs) => eval(lhs) - eval(rhs), 
+        Expr::Multiply(lhs, rhs) => eval(lhs) - eval(rhs), 
+        Expr::Divide(lhs, rhs) => eval(lhs) - eval(rhs), 
+    }
+}
