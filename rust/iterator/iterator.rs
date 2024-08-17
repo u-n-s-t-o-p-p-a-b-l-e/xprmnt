d1822bef6cef4075fbb986a8eadd78989df2fbad
unstoppable <internalminds01@gmail.com>
2024-08-18 02:03:28 +0800

    Struct counter, impl counter and iterator for counter

---
+++ rust/iterator/iterator.rs
@@ -0,0 +1,22 @@
+struct Counter {
+    count: usize,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
