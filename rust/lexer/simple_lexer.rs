808d4fd697057c604cd8006b7efe2a270db567c5
unstoppable <internalminds01@gmail.com>
2024-08-17 12:03:44 +0800

    Tokens handling

--- rust/lexer/simple_lexer.rs
+++ rust/lexer/simple_lexer.rs
@@ -16,6 +16,10 @@ fn lexer(input: &str) -> Vec<Token> {
         match ch {
             '0'..='9' => {
                 let mut num = 0;
+                while let Some('0' ..='9') = chars.peek() {
+                    num = num * 10 + chars.next().unwrap().to_digit(10).unwrap() as i32;
+                }
+                tokens.push(Token::Number(num));
             }
         }
     }
