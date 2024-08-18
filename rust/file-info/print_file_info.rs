77af7584f3123f2f2781995c4a202562514a6b25
unstoppable <internalminds01@gmail.com>
2024-08-18 19:13:38 +0800

    Convert the path to string

--- rust/file-info/print_file_info.rs
+++ rust/file-info/print_file_info.rs
@@ -2,4 +2,6 @@ use std::path::Path;
 
 fn main() {
     let exe_path = env::current_exe().expect("Failed to get executable path");
+
+    let exe_path_str = exe_path.to_str().expect("Failed to convert path to string");
 }
