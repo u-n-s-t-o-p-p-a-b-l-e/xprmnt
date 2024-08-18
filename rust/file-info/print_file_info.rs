bad669c2abc0dc6011123c154e94819cbb7f23ab
unstoppable <internalminds01@gmail.com>
2024-08-18 19:15:19 +0800

    Get the file name from the path

--- rust/file-info/print_file_info.rs
+++ rust/file-info/print_file_info.rs
@@ -4,4 +4,13 @@ fn main() {
     let exe_path = env::current_exe().expect("Failed to get executable path");
 
     let exe_path_str = exe_path.to_str().expect("Failed to convert path to string");
+
+    let file_name = Path::new(exe_path_str)
+        .file_name()
+        .expect("Failed to get file name")
+        .to_str()
+        .expect("Failed to convert file name to string");
+
+    println!("File Name: {}", file_name);
+    println!("File location: {}", exe_path_str);
 }
