bdba6ef8e75bb8159dc7995d7debe36eabfdde89
unstoppable <internalminds01@gmail.com>
2024-08-19 10:24:35 +0800

    Add a new project to the list of projects

--- python/json/process_json.py
+++ python/json/process_json.py
@@ -7,3 +7,10 @@ def read_and_modify_json(file_path):
     data['new_key'] = 'new_value'
 
     data['name'] = 'Jane Doe'
+
+    new_project = {
+        "name" : "Project Gamma",
+        "description": "A new exciting project",
+        "completed": False
+    }
+    data['projects'].append(new_project)
