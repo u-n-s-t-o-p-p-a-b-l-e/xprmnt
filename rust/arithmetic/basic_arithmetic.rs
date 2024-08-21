1b506b4edee29fb1a32e367dbccd54e6377922f0
unstoppable <internalminds01@gmail.com>
2024-08-21 20:22:02 +0800

    Test run with rustc and fix code

--- rust/arithmetic/basic_arithmetic.rs
+++ rust/arithmetic/basic_arithmetic.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let args: Vec<String> = std::env::arg().collect();
+    let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 4 {
         eprintln!("Usage: {} <num1> <operator> <num>", args[0]);
         eprintln!("Usage: use escapt for * : '*' or '\\*'");
-        return:
+        return;
     }
 
     let num1: f64 = match args[1].parse() {
@@ -33,11 +33,11 @@ fn main() {
                 eprintln!("Error: Division by zero");
                 return;
             }
-            num1 / num2,
+            num1 / num2
         }
         _ => {
             eprintln!("Error: Unsupported operator '{}'", operator);
-            return;;
+            return;
         }
     };
 
