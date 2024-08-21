70b119e805043e97dc352432ee55360b252825dd
unstoppable <internalminds01@gmail.com>
2024-08-21 20:13:45 +0800

    Invalid number

--- rust/arithmetic/basic_arithmetic.rs
+++ rust/arithmetic/basic_arithmetic.rs
@@ -3,7 +3,15 @@ fn main() {
 
     if args.len() != 4 {
         eprintln!("Usage: {} <num1> <operator> <num>", args[0]);
-        eprintln!("Usage: use escapt for * : '*' or '\\*'", args[0]);
+        eprintln!("Usage: use escapt for * : '*' or '\\*'");
         return:
     }
+
+    let num1: f64 = match args[1].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Error: Invalid number '{}'", args[1]);
+            return;
+        }
+    };
 }
