4f7878ed8019ead210afe3900939f958fb272630
unstoppable <internalminds01@gmail.com>
2024-08-22 21:04:33 +0800

    Start function

--- rust/state-machine/state_machine.rs
+++ rust/state-machine/state_machine.rs
@@ -17,5 +17,13 @@ impl StateMachine {
         }
     }
 
-    
+    fn start(&mut self) {
+        match self.state {
+            State::Start | State::Paused => {
+                self.state = State::Running;
+                println!("State changed to: {:?}", self.state);
+            }
+            _ => println!("Cannot start. Current state: {:?}", self.state),
+        }
+    }
 }
