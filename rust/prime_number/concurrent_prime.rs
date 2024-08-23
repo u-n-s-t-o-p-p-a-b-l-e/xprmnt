49a494d5b564290b6b2b3b9db9bffc74e58dd46f
unstoppable <internalminds01@gmail.com>
2024-08-23 23:16:13 +0800

    Thread

--- rust/prime_number/concurrent_prime.rs
+++ rust/prime_number/concurrent_prime.rs
@@ -8,4 +8,20 @@ fn main() {
     let (tx, rx) = mpsc::channel();
 
     let chunk_size = limit / num_threads;
+
+    for i in 0..num_threads {
+        lt thread_tx = tx.clone();
+        let start = i * chunck_size + 1;
+        let end = if i == num_threads - 1 { limit } else { (i + 1) * chunk_size };
+
+        thread::spawn(move || {
+            for num in start..=end {
+                if is_prime(num) {
+                    thread_tx.send(num).unwrap();
+                }
+            }
+        });
+    }
+
+    drop(tx);
 }
