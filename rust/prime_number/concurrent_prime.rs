255fe73572cb89ea39e9af6cc30a885ac3bfa15d
unstoppable <internalminds01@gmail.com>
2024-08-23 23:20:44 +0800

    Prime function

--- rust/prime_number/concurrent_prime.rs
+++ rust/prime_number/concurrent_prime.rs
@@ -24,4 +24,33 @@ fn main() {
     }
 
     drop(tx);
+
+    let mut primes = Vec::new();
+
+    for prime in rx {
+        primes.push(prime);
+    }
+
+    primes.sort();
+    println!("Found {} primes up to {}: {:?}", primes.len(), limit, &pprimes[0..10]);
+}
+
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n <= 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 ==  0 {
+        return false;
+    }
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+    true
 }
