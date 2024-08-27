b2ec4c0a8c0127bd11cdc0da99006b3ad02eff03
unstoppable <internalminds01@gmail.com>
2024-08-27 18:52:13 +0800

    Code fixed and added html file

--- C/http-server/multiple_client.c
+++ C/http-server/multiple_client.c
@@ -32,6 +32,15 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
     if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
