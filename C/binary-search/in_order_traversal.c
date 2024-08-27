b80dffbb5989b60acf25130df6baa6ff0cac9605
unstoppable <internalminds01@gmail.com>
2024-08-27 17:23:34 +0800

    Code completed

---
+++ C/binary-search/in_order_traversal.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) return createNode(data);
+
+    if (data < root->data) 
+        root->left = insert(root->left, data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+void inorderTraversal(Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int values[] = {5, 3, 7, 1, 4, 6, 8};
+    int n = sizeof(values) / sizeof(values[0]);
+
+    for (int i = 0; i < n; i++) {
+        root = insert(root, values[i]);
+    }
+
+    printf("Inorder traversal of the BST: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+
+}
