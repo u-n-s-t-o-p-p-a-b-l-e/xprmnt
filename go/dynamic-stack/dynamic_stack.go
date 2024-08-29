ac15672bb3bbb76dccecb5afb431f7bfd499ed85
unstoppable <internalminds01@gmail.com>
2024-08-29 23:21:19 +0800

    Pop removes the last item from the stack and returns it

--- go/dynamic-stack/dynamic_stack.go
+++ go/dynamic-stack/dynamic_stack.go
@@ -10,4 +10,15 @@ func (s *Stack) Push(item int) {
 	s.items = append(s.items, item)
 }
 
+func (s *Stack) Pop() int {
+	if len(s.items) == 0 {
+		fmt.Println("Stack is empty!")
+		return -1
+	}
+	lastIndex := len(s.items) - 1
+	item := s.items[lastIndex]
+	s.items = s.items[:lastIndex]
+	return item
+}
+
 
