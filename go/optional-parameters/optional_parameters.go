ecb329580fa3f6c6a6c25a74987be9d0fa620c89
unstoppable <internalminds01@gmail.com>
2024-08-30 15:51:55 +0800

    Using all default values

--- go/optional-parameters/optional_parameters.go
+++ go/optional-parameters/optional_parameters.go
@@ -12,4 +12,21 @@ func NewConfig(port *int, timeout *int) *Config {
 		Port: 8080,
 		Timeout: 60,
 	}
+
+	if port != nil {
+		c.Port = *porrt
+	}
+	if timeout != nil {
+		c.Timeout = *timeout
+	}
+
+	return c
+}
+
+func main() {
+	port := 9090
+	timeout := 120
+
+	config1 := NewConfig(nil, nil)
+	fmt.Printf("Config1:  Port=%d, Timeout=%d\n", config1.Port, config1.Timeout)
 }
