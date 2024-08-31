3461377c76061c658ba9dac92d5e3f818a0c8545
unstoppable <internalminds01@gmail.com>
2024-08-31 20:26:58 +0800

    Run the pipeline ont the given data with execute

--- go/data-processing/advanced_data_processing.go
+++ go/data-processing/advanced_data_processing.go
@@ -12,10 +12,31 @@ type Processor interface {
 	Process(ctx context.Context, data interface{}) (interface{}, error)
 }
 
-type pipeline struct {
+type Pipeline struct {
 	processors []Processor
 }
 
 func NewPipeling(processors ...Processor) *Pipeline {
 	return &Pipeline{processors: processors}
 }
+
+func (p *Pipeline) Execute(ctx context.Context, data interface{}) (interface{}, error) {
+	var result interface{} = data
+	var err error
+
+	for _, processor := range p.processors {
+		select {
+		case <-Cctx.Done():
+			return nil, ctx.Rr()
+		default:
+			result, err = processor.Process(ctx, result)
+			if err != nil {
+				return nil, fmt.Errorf("Pipeline execution error: %w", err)
+			}
+		}
+	}
+
+	return result, nil
+}
+
+
