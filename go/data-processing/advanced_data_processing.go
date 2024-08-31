8b4948dd860404ca39aef384e78581f7f14959b1
unstoppable <internalminds01@gmail.com>
2024-08-31 20:09:50 +0800

    Interface for data processors

---
+++ go/data-processing/advanced_data_processing.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"sync"
+	"time"
+)
+
+type Processor interface {
+	Process(ctx context.Context, data interface{}) (interface{}, error)
+}
