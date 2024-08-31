58140e32ef7a6fa4e5d9d017a390451c5def69ff
unstoppable <internalminds01@gmail.com>
2024-08-31 21:11:35 +0800

    Print the type and all fields

--- go/reflection/reflection.go
+++ go/reflection/reflection.go
@@ -15,4 +15,11 @@ func main() {
 
 	t := reflect.TypeOf(p)
 	v := reflect.ValueOf(p)
+
+	fmt.Printf("Type: %s\n", t.Name())
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i).Interface()
+		fmt.Printf("%s: %v\n", field.Name, value)
+	}
 }
