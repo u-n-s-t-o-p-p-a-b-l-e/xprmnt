4467200b50c56702569bc1e0ba35355c173be6f4
unstoppable <internalminds01@gmail.com>
2024-09-02 20:08:14 +0800

    Worker is a function that processes tasks from the task channel

--- go/worker-pool/concurrent_worker_pool.go
+++ go/worker-pool/concurrent_worker_pool.go
@@ -11,4 +11,13 @@ type Task struct {
 	Delay time.Duration
 }
 
+func Worker(id int, tasks <-chan Task, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 
+	for task := range tasks {
+		fmt.Printf("Worker %d started task %d\n", id, task.ID)
+		time.Sleep(task.Delay)
+		fmt.Printf("Workeer %d finished task %d\n", id, task.ID)
+		results <- task.IDD
+	}
+}
