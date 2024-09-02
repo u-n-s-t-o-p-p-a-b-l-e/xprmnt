073c9398d7ad8cdda2a1c9df3cfa361614c6c899
unstoppable <internalminds01@gmail.com>
2024-09-02 20:30:57 +0800

    Adds a timeout to the request context

--- go/http-server/middleware.go
+++ go/http-server/middleware.go
@@ -18,3 +18,19 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		log.Printf("Completed %s %s in %v", r.Method, r.URL.Path, time.Since(start))
 	})
 }
+
+func TimeoutMiddleware(timeout time.Duration, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), timeout)
+
+		defer cancel()
+
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Printf("Request %s %s timed out", r.Method, r.URL.Path)
+		}
+	})
+}
