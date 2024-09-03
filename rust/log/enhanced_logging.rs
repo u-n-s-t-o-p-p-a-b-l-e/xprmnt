6ffce3d95625a564f28a94a3f92f318f2d90fbfc
unstoppable <internalminds01@gmail.com>
2024-09-03 21:25:08 +0800

    Enum LogLevel

---
+++ rust/log/enhanced_logging.rs
@@ -0,0 +1,11 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(PartialEq, PartialOrd)]
+enum LogLevel {
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG,
+}
