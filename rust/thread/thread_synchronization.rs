6e274d178e78538b0c6a638c7427e07daad5cf99
unstoppable <internalminds01@gmail.com>
2024-09-04 15:41:39 +0800

    Wait for all threads to finish

--- rust/thread/thread_synchronization.rs
+++ rust/thread/thread_synchronization.rs
@@ -9,6 +9,13 @@ fn main() {
         let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
-        })
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
