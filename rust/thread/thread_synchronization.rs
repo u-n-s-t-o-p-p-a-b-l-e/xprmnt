a0768174e3b494fe805365f8572947f2fa20c490
unstoppable <internalminds01@gmail.com>
2024-09-04 15:39:35 +0800

    Spawn 10 threads

--- rust/thread/thread_synchronization.rs
+++ rust/thread/thread_synchronization.rs
@@ -3,4 +3,12 @@ use std::thread;
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+
+        })
+    }
 }
