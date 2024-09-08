072f561f4bac314ed3c42ad5ba857f03dc80b3a6
unstoppable <internalminds01@gmail.com>
2024-09-08 23:55:16 +0800

    Implement basic operations for the binary search tree

--- rust/binary-tree/binary_search_tree.rs
+++ rust/binary-tree/binary_search_tree.rs
@@ -5,3 +5,13 @@ struct Node<T> {
     left: Option<Box<Node<T>>>,
     right: Oprion<Box<Node<T>>>,
 }
+
+impl<T:Ord + std::fmt::Display> Node<T> {
+    fn new(value: T) -> Self {
+        Node {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+}
