3f74386bf75f83bcd4355ef5aebcae1e69b0b89d
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-12 07:02:01 -0400

    Implement a method for creating a new instance of WordIterator

--- rust/custom-iter/custom_word_iter.rs
+++ rust/custom-iter/custom_word_iter.rs
@@ -2,3 +2,9 @@ struct WordIterator<'a> {
     text: &'a str,
     current_pos: usize,
 }
+
+impl<'a> WordIterator<'a> {
+    fn new(text: &'a str) -> WordIterator<'a> {
+        WordIterator { text, current_pos: 0 }
+    }
+}
