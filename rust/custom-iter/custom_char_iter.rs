6e77ab8502f899c5bae5fb87819ad1dd7db4286d
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-13 11:06:26 -0400

    Define how the next character in the text is produced

--- rust/custom-iter/custom_char_iter.rs
+++ rust/custom-iter/custom_char_iter.rs
@@ -8,3 +8,13 @@ impl<'a> CharIterator<'a> {
         CharIterator { text, current_post: 0 }
     }
 }
+
+impl<'a> CharIterrator<'a> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_pos >= self.text.len() {
+            return None;
+        }
+    }
+}
