26ae0c04664d8dabf8396252c10420ff09813c4f
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-13 11:29:43 -0400

    Retrieve a value from the cache if it hasn't expired

--- rust/cache/custom_cache.rs
+++ rust/cache/custom_cache.rs
@@ -23,4 +23,22 @@ impl<K: std::cmp::Eq + std::hash::Hash, V> Cache<K, V> {
         let expiration = ttl.map(|t| Instant::now() + t).or(self.default_ttl.map(|t| Instant::now() +t));
         self.store.insert(key, CacheEntry { value, expiration });
     }
+
+    fn get(&mut self, key: &K) -> Option<&V> {
+        let expired = {
+            if let Some(entry) = self.store.get(key) {
+                if let Some(expiration) = entry.expiration {
+                    if Instant::now() >= expiration {
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            } else {
+                return None;
+            }
+        };
+    }
 }
