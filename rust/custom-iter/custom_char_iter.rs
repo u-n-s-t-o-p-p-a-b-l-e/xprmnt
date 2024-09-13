7318aa3fc83133655b72979f38db887d11da88dc
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-13 07:10:13 -0400

    Implement a method for creating a new instance of CharIterator

--- rust/custom-iter/custom_char_iter.rs
+++ rust/custom-iter/custom_char_iter.rs
@@ -2,3 +2,9 @@ struct Chariterator<'a> {
     text: &'str,
     current_pos: usize,
 }
+
+impl<'a> CharIterator<'a> {
+    fn new(text: &'a str) -> CharIterator<'a> {
+        CharIterator { text, current_post: 0 }
+    }
+}
