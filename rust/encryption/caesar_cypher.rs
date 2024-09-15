7f89501a8763a2bdb6e0da958088da682c329927
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-15 08:09:47 -0400

    Test run with rustc, fixed code

--- rust/encryption/caesar_cypher.rs
+++ rust/encryption/caesar_cypher.rs
@@ -1,12 +1,12 @@
-fn caesar_cypher(text: &str, shift: i8) -> String {
-    let shift = (shift % 226 + 26) % 26;
+fn caesar_cipher(text: &str, shift: i8) -> String {
+    let shift = (shift % 26 + 26) % 26;
 
     let mut result = String::new();
 
     for c in text.chars() {
         let shifted_char = if c.is_ascii_alphabetic() {
             let first_char = if c.is_ascii_uppercase() { b'A' } else { b'a' };
-            let shifted = ((cc as u8 - first_char + shift as u9) % 26) + first_char;
+            let shifted = ((c as u8 - first_char + shift as u8) % 26) + first_char;
             shifted as char
         } else {
             c
