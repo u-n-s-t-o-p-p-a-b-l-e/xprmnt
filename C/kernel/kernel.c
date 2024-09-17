328da1e283b621881dcec551e672a769daf36e35
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-17 09:19:08 -0400

    Print char

--- C/kernel/kernel.c
+++ C/kernel/kernel.c
@@ -1,2 +1,7 @@
 unsigned short *vga_buffer = (unsigned short *)0xB8000;
 int vga_index = 0;
+
+void print_char(char ch, int color) {
+    vga_buffer[vga_index] = (unsigned short) (ch | color << 8);
+    vga_index++;
+}
