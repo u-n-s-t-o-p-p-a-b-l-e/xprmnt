1317011b358fa1e47fd90d1234a5a661688ca238
u-n-s-t-o-p-p-a-b-l-e <internalminds01@gmail.com>
2024-09-17 09:21:14 -0400

    Print string

--- C/kernel/kernel.c
+++ C/kernel/kernel.c
@@ -5,3 +5,9 @@ void print_char(char ch, int color) {
     vga_buffer[vga_index] = (unsigned short) (ch | color << 8);
     vga_index++;
 }
+
+void print_string(const char *str, int color) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        print_char(str[i], color);
+    }
+}
