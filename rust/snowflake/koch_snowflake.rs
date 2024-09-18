6597eb719ec24734ec626089c67a895b75a86135
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-18 18:25:07 +0800

    Draw a straight line

--- rust/snowflake/koch_snowflake.rs
+++ rust/snowflake/koch_snowflake.rs
@@ -1,1 +1,9 @@
 use std::f64::consts::PI;
+
+fn draw_line(x1: f64, y1: f64, x2: f64, y2: f64, depth:  usize) {
+    if depth == 0 {
+        println!("Line from ({:.2}, {:.2} to {:.2}, {:.2})", x1, y1, x2, y2);
+    } else {
+
+    }
+}
