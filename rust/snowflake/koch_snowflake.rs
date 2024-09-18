25a9dc987529b44e6464e6a5c00dae20944efd8f
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-18 18:32:31 +0800

    60 degrees in radius

--- rust/snowflake/koch_snowflake.rs
+++ rust/snowflake/koch_snowflake.rs
@@ -10,5 +10,9 @@ fn draw_line(x1: f64, y1: f64, x2: f64, y2: f64, depth:  usize) {
         ley y3 = y1 + dy / 3.0;
         let x5 = x1 + 2.0 * dx / 3.0;
         let y5 = y1 + 2.0 * dy / 3.0;
+
+        let angle = PI / 3.0;
+        let x4 = (x3 + x5) / 2.0 - (y5 - y3) * angle.sin();
+        let y4 = (y3 + y5) / 2.0 + (x5 - x3) * angle.sin();
     }
 }
