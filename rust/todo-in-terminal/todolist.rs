f931c755c498ba6ad19f3217818a3f62e9104f34
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-19 22:21:44 +0800

    Starting instructions

--- rust/todo-in-terminal/todolist.rs
+++ rust/todo-in-terminal/todolist.rs
@@ -4,4 +4,18 @@ use std::path::Path;
 
 fn display_tasks() -> io::Result<()> {
     let path = "tasks.txt";
+
+    if Path::new(&path).exitst() {
+        let file = File::open(path)?;
+        let reader = io::BufReader::new(file);
+
+        println!("Your To-Do List:");
+        for (i, line) in reader.lines().enumerate() {
+            println!("{}. {}", i + 1, line?);
+        }
+    } else {
+        println!("No tasks available. Add a task to get started.");
+    }
+
+    Ok(())
 }
