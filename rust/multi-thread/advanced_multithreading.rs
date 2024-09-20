b8f6827e3ac0d381910aafac2394ecc122a07bb0
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-20 22:37:44 +0800

    Simulate work

--- rust/multi-thread/advanced_multithreading.rs
+++ rust/multi-thread/advanced_multithreading.rs
@@ -4,4 +4,15 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+
+    for i in 0..3 {
+        let tx_clone = tx.clone();
+        thread::spawn(move || {
+            for j in 0..5 {
+                let message = format!("Producer {}: Message {} ", i, j);
+                tx_clone.send(message).unwrap();
+                thread::sleep(Duration::from_millis(100));
+            }
+        });
+    }
 }
