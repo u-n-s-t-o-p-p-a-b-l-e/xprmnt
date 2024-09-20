4d5c5a05a06c8b59936700a0688d485ec39b0d2b
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-20 22:43:39 +0800

    Take the first 10 fibonacci numbers

---
+++ rust/fibonacci/custom_infinite_fibonacci.rs
@@ -0,0 +1,29 @@
+struct Fibonacci {
+    curr: u64,
+    next: u64,
+}
+
+impl Fibonacci {
+    fn new() -> Fibonacci {
+        Fibonacci { curr: 0, next: 1 }
+    }
+}
+
+impl Iterator for Fibonacci {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_next = self.curr + self.next;
+        self.curr = self.next;
+        self.next = new_next;
+        some(self.curr)
+    }
+}
+
+fn main() {
+    let fib = Fibonacci::new();
+
+    for num in fib.take(10) {
+        println!("{}", num);
+    }
+}
