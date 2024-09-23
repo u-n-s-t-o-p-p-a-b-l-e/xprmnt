a6dc7dc4447a3a38ceb1a92ab0c680908b648a2c
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-09-23 14:49:58 +0800

    Insert function

--- rust/binary-tree/lockless_concurrent.rs
+++ rust/binary-tree/lockless_concurrent.rs
@@ -16,4 +16,25 @@ impl<T: Ord + Clone> Node<T> {
             right: AtomicPtr::new(ptr::null_mut()),
         }
     }
+
+    fn insert(&self, value: &T) {
+        let node = Box::into_raw(Box::new(Node::new(value.clone())));
+        let mut current = self;
+
+        loop {
+            match current.value.cmp(value) {
+                CmpOrdering::Greater => {
+                    match current.left.compare_exchange(
+                        ptr::null_mut(),
+                        node,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
+                        Ok(_) => break,
+                        Err(next) => current = unsafe { &*next },
+                    }
+                }
+            }
+        }
+    }
 }
