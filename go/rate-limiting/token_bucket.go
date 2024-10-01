c3b87d7d99dc1aaeadcfec25f9a418a692013c69
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-10-01 20:24:36 +0800

    Creates new token bucket, start to refill process

--- go/rate-limiting/token_bucket.go
+++ go/rate-limiting/token_bucket.go
@@ -12,3 +12,15 @@ type TokenBucket struct {
 	ticker *time.Ticker
 	quit chan struct{}
 }
+
+func NewTokenBucket(rate, max int) *TokenBucket {
+	tb := &TokenBucket{
+		rate: rate,
+		max:   max,
+		bucket: max,
+		ticker: time.NewTicker(1 * time.second),
+		quit: make(chan struct{}),
+	}
+	go tb.refill()
+	return tb
+}
