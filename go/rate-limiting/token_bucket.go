19e2cf19ffe704abc5589ec91db4e7975d32d28f
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-10-01 20:28:06 +0800

    Refill periodically refills the token bucket

--- go/rate-limiting/token_bucket.go
+++ go/rate-limiting/token_bucket.go
@@ -15,12 +15,29 @@ type TokenBucket struct {
 
 func NewTokenBucket(rate, max int) *TokenBucket {
 	tb := &TokenBucket{
-		rate: rate,
-		max:   max,
+		rate:   rate,
+		max:    max,
 		bucket: max,
-		ticker: time.NewTicker(1 * time.second),
-		quit: make(chan struct{}),
+		ticker: time.NewTicker(1 * time.Second),
+		quit:   make(chan struct{}),
 	}
 	go tb.refill()
 	return tb
+}
+
+func (tb *TokenBucket) refill() {
+	for {
+		select {
+		case <-tb.ticker.C:
+			if tb.bucket < tb.max {
+				tb.bucket += tb.rate
+				if tb.bucket > tb.max {
+					tb.bucket = tb.max
+				}
+				fmt.Printf("Bucket refilled: %d tokens available\n", tb.bucket)
+			}
+		case <-tb.quit:
+			return
+		}
+	}
 }
