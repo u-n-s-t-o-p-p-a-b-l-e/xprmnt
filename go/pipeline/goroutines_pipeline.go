20d258abfb852d0cdd0ceb424afcaf1290f40756
u-n-s-t-o-p-p-b-l-e <internalminds01@gmail.com>
2024-10-22 22:49:05 +0800

    Generates numbers and sends them to a channel

---
+++ go/pipeline/goroutines_pipeline.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func gen(nums ..int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, num := range nums {
+			out <- num
+		}
+		close(out)
+	}()
+	return out
+}
